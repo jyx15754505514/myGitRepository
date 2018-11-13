4c5e8db57b09e7c21fd520c3b96bcc3fdc60a6b4
yanjin <yanjin@yuanh.net>
2018-11-13 23:44:46 +0800

    修改entrance-fin-web

--- entrance-module/entrance-fin-web/src/main/java/com/ccicnavi/bims/fin/EntranceFinWebApplication.java
+++ entrance-module/entrance-fin-web/src/main/java/com/ccicnavi/bims/fin/EntranceFinWebApplication.java
@@ -1,9 +1,15 @@
 package com.ccicnavi.bims.fin;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = "com.ccicnavi.bims.fin.controller")
+@Configuration
+@EnableApolloConfig
+@ComponentScan(basePackages = { "com.ccicnavi.bims.fin.*" }) // 将该包下的文件纳入容器中
+@SpringBootApplication
 public class EntranceFinWebApplication {
 
     public static void main(String[] args) {
