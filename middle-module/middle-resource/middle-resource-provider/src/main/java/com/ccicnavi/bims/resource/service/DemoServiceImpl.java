b092c45b04ba9ee0c99f71632630d2e235350831
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-13 20:17:24 +0800

    修改提交

--- middle-module/middle-resource/middle-resource-provider/src/main/java/com/ccicnavi/bims/resource/service/DemoServiceImpl.java
+++ middle-module/middle-resource/middle-resource-provider/src/main/java/com/ccicnavi/bims/resource/service/DemoServiceImpl.java
@@ -1,9 +1,9 @@
-package com.ccicnavi.bims.resource.pojo;
+package com.ccicnavi.bims.resource.service;
 
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-import com.ccicnavi.bims.resource.pojo.api.DemoService;
+import com.ccicnavi.bims.resource.api.DemoService;
 
 @Service(version = "1.0.0")
 public class DemoServiceImpl implements DemoService {
