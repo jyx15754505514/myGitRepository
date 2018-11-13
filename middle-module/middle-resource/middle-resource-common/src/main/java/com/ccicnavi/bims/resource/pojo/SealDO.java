1faf9b7b3def4179c6dc4819c3b0c9992f53f01f
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-13 20:36:07 +0800

    修改提交

--- middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/SealDO.java
+++ middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/SealDO.java
@@ -1,8 +1,15 @@
 package com.ccicnavi.bims.resource.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
 
+@Getter
+@Setter
+@ToString
 public class SealDO implements Serializable{
     private String selUuid;
 
@@ -48,179 +55,4 @@ public class SealDO implements Serializable{
 
     private String extend3;
 
-    public String getSelUuid() {
-        return selUuid;
-    }
-
-    public void setSelUuid(String selUuid) {
-        this.selUuid = selUuid == null ? null : selUuid.trim();
-    }
-
-    public String getSelName() {
-        return selName;
-    }
-
-    public void setSelName(String selName) {
-        this.selName = selName == null ? null : selName.trim();
-    }
-
-    public String getSelEngName() {
-        return selEngName;
-    }
-
-    public void setSelEngName(String selEngName) {
-        this.selEngName = selEngName == null ? null : selEngName.trim();
-    }
-
-    public String getSelCertName() {
-        return selCertName;
-    }
-
-    public void setSelCertName(String selCertName) {
-        this.selCertName = selCertName == null ? null : selCertName.trim();
-    }
-
-    public String getSelEnmUuid() {
-        return selEnmUuid;
-    }
-
-    public void setSelEnmUuid(String selEnmUuid) {
-        this.selEnmUuid = selEnmUuid == null ? null : selEnmUuid.trim();
-    }
-
-    public String getSelCount() {
-        return selCount;
-    }
-
-    public void setSelCount(String selCount) {
-        this.selCount = selCount == null ? null : selCount.trim();
-    }
-
-    public String getSelBatchNo() {
-        return selBatchNo;
-    }
-
-    public void setSelBatchNo(String selBatchNo) {
-        this.selBatchNo = selBatchNo == null ? null : selBatchNo.trim();
-    }
-
-    public String getSelStartNumber() {
-        return selStartNumber;
-    }
-
-    public void setSelStartNumber(String selStartNumber) {
-        this.selStartNumber = selStartNumber == null ? null : selStartNumber.trim();
-    }
-
-    public String getSelEndNumber() {
-        return selEndNumber;
-    }
-
-    public void setSelEndNumber(String selEndNumber) {
-        this.selEndNumber = selEndNumber == null ? null : selEndNumber.trim();
-    }
-
-    public String getSelPerson() {
-        return selPerson;
-    }
-
-    public void setSelPerson(String selPerson) {
-        this.selPerson = selPerson == null ? null : selPerson.trim();
-    }
-
-    public String getSelKeeper() {
-        return selKeeper;
-    }
-
-    public void setSelKeeper(String selKeeper) {
-        this.selKeeper = selKeeper == null ? null : selKeeper.trim();
-    }
-
-    public String getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(String isDeleted) {
-        this.isDeleted = isDeleted == null ? null : isDeleted.trim();
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public String getCreatedUuid() {
-        return createdUuid;
-    }
-
-    public void setCreatedUuid(String createdUuid) {
-        this.createdUuid = createdUuid == null ? null : createdUuid.trim();
-    }
-
-    public Date getUpdatedTime() {
-        return updatedTime;
-    }
-
-    public void setUpdatedTime(Date updatedTime) {
-        this.updatedTime = updatedTime;
-    }
-
-    public String getUpdatedName() {
-        return updatedName;
-    }
-
-    public void setUpdatedName(String updatedName) {
-        this.updatedName = updatedName == null ? null : updatedName.trim();
-    }
-
-    public String getBusinessLine() {
-        return businessLine;
-    }
-
-    public void setBusinessLine(String businessLine) {
-        this.businessLine = businessLine == null ? null : businessLine.trim();
-    }
-
-    public String getOrgUuid() {
-        return orgUuid;
-    }
-
-    public void setOrgUuid(String orgUuid) {
-        this.orgUuid = orgUuid == null ? null : orgUuid.trim();
-    }
-
-    public String getAppSysUuid() {
-        return appSysUuid;
-    }
-
-    public void setAppSysUuid(String appSysUuid) {
-        this.appSysUuid = appSysUuid == null ? null : appSysUuid.trim();
-    }
-
-    public String getExtend1() {
-        return extend1;
-    }
-
-    public void setExtend1(String extend1) {
-        this.extend1 = extend1 == null ? null : extend1.trim();
-    }
-
-    public String getExtend2() {
-        return extend2;
-    }
-
-    public void setExtend2(String extend2) {
-        this.extend2 = extend2 == null ? null : extend2.trim();
-    }
-
-    public String getExtend3() {
-        return extend3;
-    }
-
-    public void setExtend3(String extend3) {
-        this.extend3 = extend3 == null ? null : extend3.trim();
-    }
 }
--- middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/TemplateDO.java
+++ middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/TemplateDO.java
@@ -1,8 +1,16 @@
 package com.ccicnavi.bims.resource.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
 
+
+@Getter
+@Setter
+@ToString
 public class TemplateDO implements Serializable {
     private String templateUuid;
 
@@ -38,139 +46,5 @@ public class TemplateDO implements Serializable {
 
     private String extend3;
 
-    public String getTemplateUuid() {
-        return templateUuid;
-    }
 
-    public void setTemplateUuid(String templateUuid) {
-        this.templateUuid = templateUuid == null ? null : templateUuid.trim();
-    }
-
-    public String getDictTypeUuid() {
-        return dictTypeUuid;
-    }
-
-    public void setDictTypeUuid(String dictTypeUuid) {
-        this.dictTypeUuid = dictTypeUuid == null ? null : dictTypeUuid.trim();
-    }
-
-    public String getTemplateName() {
-        return templateName;
-    }
-
-    public void setTemplateName(String templateName) {
-        this.templateName = templateName == null ? null : templateName.trim();
-    }
-
-    public String getTemplateFileUuid() {
-        return templateFileUuid;
-    }
-
-    public void setTemplateFileUuid(String templateFileUuid) {
-        this.templateFileUuid = templateFileUuid == null ? null : templateFileUuid.trim();
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments == null ? null : comments.trim();
-    }
-
-    public String getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(String isDeleted) {
-        this.isDeleted = isDeleted == null ? null : isDeleted.trim();
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public String getCreatedUuid() {
-        return createdUuid;
-    }
-
-    public void setCreatedUuid(String createdUuid) {
-        this.createdUuid = createdUuid == null ? null : createdUuid.trim();
-    }
-
-    public String getCreatedName() {
-        return createdName;
-    }
-
-    public void setCreatedName(String createdName) {
-        this.createdName = createdName == null ? null : createdName.trim();
-    }
-
-    public Date getUpdatedTime() {
-        return updatedTime;
-    }
-
-    public void setUpdatedTime(Date updatedTime) {
-        this.updatedTime = updatedTime;
-    }
-
-    public String getUpdatedName() {
-        return updatedName;
-    }
-
-    public void setUpdatedName(String updatedName) {
-        this.updatedName = updatedName == null ? null : updatedName.trim();
-    }
-
-    public String getBusinessLine() {
-        return businessLine;
-    }
-
-    public void setBusinessLine(String businessLine) {
-        this.businessLine = businessLine == null ? null : businessLine.trim();
-    }
-
-    public String getOrgUuid() {
-        return orgUuid;
-    }
-
-    public void setOrgUuid(String orgUuid) {
-        this.orgUuid = orgUuid == null ? null : orgUuid.trim();
-    }
-
-    public String getAppSysUuid() {
-        return appSysUuid;
-    }
-
-    public void setAppSysUuid(String appSysUuid) {
-        this.appSysUuid = appSysUuid == null ? null : appSysUuid.trim();
-    }
-
-    public String getExtend1() {
-        return extend1;
-    }
-
-    public void setExtend1(String extend1) {
-        this.extend1 = extend1 == null ? null : extend1.trim();
-    }
-
-    public String getExtend2() {
-        return extend2;
-    }
-
-    public void setExtend2(String extend2) {
-        this.extend2 = extend2 == null ? null : extend2.trim();
-    }
-
-    public String getExtend3() {
-        return extend3;
-    }
-
-    public void setExtend3(String extend3) {
-        this.extend3 = extend3 == null ? null : extend3.trim();
-    }
 }
