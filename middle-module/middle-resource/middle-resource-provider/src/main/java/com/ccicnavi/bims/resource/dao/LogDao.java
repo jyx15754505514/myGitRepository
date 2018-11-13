e1a95ee5b9568b7c09debb6fe47c87a5701f3e31
mengzijie <mengzijie@yuanh.net>
2018-11-14 00:11:57 +0800

    日志管理

---
+++ middle-module/middle-resource/middle-resource-provider/src/main/java/com/ccicnavi/bims/resource/dao/LogDao.java
@@ -0,0 +1,17 @@
+package com.ccicnavi.bims.resource.dao;
+
+//import com.ccicnavi.bims.resource.service.pojo.LogDO;
+//import java.util.List;
+
+/**
+ * 日志管理
+ * @Date 2018/11/13 19 54
+ * @Auther mzj
+ */
+public interface LogDao {
+
+    /**查询所有的日志*/
+    //List<LogDO> listLog(LogDO logDO);
+    /**获取指定的日志*/
+    //LogDO getLog(LogDO logDO);
+}
