374d8fff4e462191922c3c91f38e3e5e854799cf
StaticAnn <37128990+StaticAnn@users.noreply.github.com>
2018-11-14 16:57:11 +0800

    增加客户特殊需求信息（SpecialNeedDO）,分包方资质（SubcQualifiDO）,分包方信息的DO（SubcontractorDO），Service,ServiceImpl,Dao,DaoImpl和分包方资质,分包方信息的Eql

---
+++ middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/dao/Impl/SpecialNeedDaoImpl.java
@@ -0,0 +1,28 @@
+package com.ccicnavi.bims.customer.dao.Impl;
+
+import com.ccicnavi.bims.customer.dao.SpecialNeedDao;
+import com.ccicnavi.bims.customer.pojo.SpecialNeedDO;
+
+import java.util.List;
+
+public class SpecialNeedDaoImpl implements SpecialNeedDao {
+    @Override
+    public List<SpecialNeedDO> listSpecialNeed(SpecialNeedDO specialNeed) {
+        return null;
+    }
+
+    @Override
+    public int saveSpecialNeed(SpecialNeedDO specialNeed) {
+        return 0;
+    }
+
+    @Override
+    public int removeSpecialNeed(String needUuid) {
+        return 0;
+    }
+
+    @Override
+    public int updateSpecialNeed(SpecialNeedDO specialNeed) {
+        return 0;
+    }
+}
---
+++ middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/provider/SubcontractorServiceImpl.java
@@ -0,0 +1,33 @@
+package com.ccicnavi.bims.customer.provider;
+
+import com.ccicnavi.bims.customer.api.SubcontractorService;
+import com.ccicnavi.bims.customer.pojo.SubcontractorDO;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+public class SubcontractorServiceImpl implements SubcontractorService{
+
+    @Resource
+    SubcontractorService subcontractorService;
+
+    @Override
+    public List<SubcontractorDO> listSubcontractor(SubcontractorDO subcontractor) {
+        return subcontractorService.listSubcontractor(subcontractor);
+    }
+
+    @Override
+    public int saveSubcontractor(SubcontractorDO subcontractor) {
+        return subcontractorService.saveSubcontractor(subcontractor);
+    }
+
+    @Override
+    public int removeSubcontractor(String subcontractorUuid) {
+        return subcontractorService.removeSubcontractor(subcontractorUuid);
+    }
+
+    @Override
+    public int updateSubcontractor(SubcontractorDO subcontractor) {
+        return subcontractorService.updateSubcontractor(subcontractor);
+    }
+}
