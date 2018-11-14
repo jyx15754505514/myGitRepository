1c338ce35824fb8cff98e9aa501ac7de5ed6c7fd
柴言顺 <18333149648@163.com>
2018-11-14 17:17:46 +0800

    ADD 人员工作，工作经历，人员资格证书   Do dao DaoImpl Service ServiceImpl

---
+++ middle-module/middle-resource/middle-resource-provider/src/main/java/com/ccicnavi/bims/resource/service/ResQualificationCertServiceImpl.java
@@ -0,0 +1,59 @@
+package com.ccicnavi.bims.resource.service;
+
+import com.ccicnavi.bims.resource.api.ResQualificationCertService;
+import com.ccicnavi.bims.resource.dao.ResQualificationCertDao;
+import com.ccicnavi.bims.resource.pojo.ResQualificationCertDO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @Author:
+ * @Description:
+ * @Date: Created in ${Time} 2018/11/14
+ * @Modified By:
+ */
+public class ResQualificationCertServiceImpl implements ResQualificationCertService {
+    @Autowired
+    ResQualificationCertDao resQualificationCertDao;
+
+    /**
+     *查询人员资格证书
+     */
+    @Override
+    public List<ResQualificationCertDO> listResQualificationCertDO(ResQualificationCertDO resQualificationCertDO) throws Exception {
+        return resQualificationCertDao.listResQualificationCertDO(resQualificationCertDO);
+    }
+
+    /**
+     *新增人员资格证书
+     */
+    @Override
+    public Integer insertResQualificationCertDO(ResQualificationCertDO resQualificationCertDO) throws Exception {
+        return resQualificationCertDao.insertResQualificationCertDO(resQualificationCertDO);
+    }
+
+    /**
+     *更改人员资格证书
+     */
+    @Override
+    public Integer updateResQualificationCertDO(ResQualificationCertDO resQualificationCertDO) throws Exception {
+        return resQualificationCertDao.updateResQualificationCertDO(resQualificationCertDO);
+    }
+
+    /**
+     *删除人员资格证书
+     */
+    @Override
+    public Integer deleteResQualificationCertDO(ResQualificationCertDO resQualificationCertDO) throws Exception {
+        return resQualificationCertDao.deleteResQualificationCertDO(resQualificationCertDO);
+    }
+
+    /**
+     *根据主键查询人员资格证书
+     */
+    @Override
+    public ResQualificationCertDO getResQualificationCertDO(ResQualificationCertDO resQualificationCertDO) throws Exception {
+        return resQualificationCertDao.getResQualificationCertDO(resQualificationCertDO);
+    }
+}
