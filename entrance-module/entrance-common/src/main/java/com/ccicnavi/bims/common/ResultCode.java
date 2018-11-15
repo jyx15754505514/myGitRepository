50603d50f8155215ceea818b259f9ecb0f58b182
zhangqingqing <2067838559@qq.com>
2018-11-15 23:17:20 +0800

    提醒设置返回消息编码和内容

--- entrance-module/entrance-common/src/main/java/com/ccicnavi/bims/common/ResultCode.java
+++ entrance-module/entrance-common/src/main/java/com/ccicnavi/bims/common/ResultCode.java
@@ -37,6 +37,12 @@ public enum ResultCode {
     UPDATE_FAILURE(30013, "修改失败"),
 
 
+    RENIND_LIST_RENINDGET(30101, "根据id查询提醒设置失败!"),
+    RENIND_LIST_RENINDLIST(30102, "查询提醒设置失败!"),
+    RENIND_LIST_RENINDINSERT(30103, "新增提醒设置失败!"),
+    RENIND_LIST_RENINDUPDATE(30104, "修改提醒设置失败!"),
+    RENIND_LIST_RENINDDELETE(30105, "删除提醒设置失败!"),
+
 
     /* 系统错误：40001-49999 */
     SYSTEM_INNER_ERROR(40001, "系统繁忙，请稍后重试"),
