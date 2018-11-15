d047b7bf366924c04e24203a71f69523f8febdc8
mengzijie <mengzijie@yuanh.net>
2018-11-15 22:14:00 +0800

    mod 地区管理

--- entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/controller/AreaController.java
+++ entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/controller/AreaController.java
@@ -4,8 +4,11 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.ccicnavi.bims.common.ResultT;
 import com.ccicnavi.bims.system.pojo.AreaDO;
 import com.ccicnavi.bims.system.service.api.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -13,7 +16,9 @@ import java.util.List;
 @RequestMapping("area")
 public class AreaController {
 
-    @Reference(url = "dubbo://127.0.0.1:20881")
+    private final static Logger log = LoggerFactory.getLogger(AreaController.class);
+
+    @Reference(timeout = 30000,url = "dubbo://127.0.0.1:20881")
     AreaService areaService;
 
     /* *
@@ -23,10 +28,16 @@ public class AreaController {
      * @Param [areaDO]
      * @Return com.ccicnavi.bims.common.ResultT
      */
-    @RequestMapping("listArea")
+    @RequestMapping(value = "listArea",method = RequestMethod.POST)
     public ResultT listArea(@RequestBody AreaDO areaDO){
         ResultT resultT = new ResultT();
-        List<AreaDO> areaDOS = areaService.listArea(areaDO);
+        List<AreaDO> areaDOS = null;
+        try {
+            areaDOS = areaService.listArea(areaDO);
+        } catch (Exception e) {
+            log.debug("查询地区失败",e);
+            e.printStackTrace();
+        }
         resultT.success(areaDOS);
         return resultT;
     }
