79804b6c46f46593f2bd128cf7255ef2477035cb
PanYida <panyida@yuanh.net>
2018-11-15 21:44:04 +0800

    ADD 设备领用归还 EquipUse 第三方api接口，实体类，Dao，eql

---
+++ middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/EquipUseDO.java
@@ -0,0 +1,51 @@
+package com.ccicnavi.bims.resource.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @program: bims-backend
+ * @description: 设备领用归还信息DO
+ * @author: panyida
+ * @create: 2018-11-15 19:32
+ **/
+@Getter
+@Setter
+@ToString
+public class EquipUseDO implements Serializable {
+    private String equipUseUuid;
+
+    private String equipUuid;
+
+    private Date useDate;
+
+    private String usePerson;
+
+    private String useDept;
+
+    private Date backDate;
+
+    private String businessLine;
+
+    private String comments;
+
+    private String isDeleted;
+
+    private Date createdTime;
+
+    private String createdUuid;
+
+    private String createdName;
+
+    private Date updatedTime;
+
+    private String updatedName;
+
+    private String orgUuid;
+
+    private String appSysUuid;
+}
