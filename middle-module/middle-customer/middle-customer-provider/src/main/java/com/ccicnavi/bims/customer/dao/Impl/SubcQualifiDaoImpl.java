40632ea3c8677deda71de710e178497a7de57f2d
StaticAnn <37128990+StaticAnn@users.noreply.github.com>
2018-11-15 15:42:48 +0800

    测试了分包方信息(crm_subcontractor),分包方资质(crm_subc_qualification),客户特殊需求信息（crm_special_need）的基础SQL语句后对相应Eql文件做了修改

--- middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/dao/Impl/SubcQualifiDaoImpl.java
+++ middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/dao/Impl/SubcQualifiDaoImpl.java
@@ -3,6 +3,8 @@ package com.ccicnavi.bims.customer.dao.Impl;
 import com.ccicnavi.bims.customer.dao.SubcQualifiDao;
 import com.ccicnavi.bims.customer.pojo.SubcQualifiDO;
 import com.ccicnavi.bims.customer.util.EqlUtils;
+import org.junit.jupiter.api.Test;
+import org.n3r.eql.Eql;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -38,4 +40,58 @@ public class SubcQualifiDaoImpl implements SubcQualifiDao {
     public SubcQualifiDO getSubcQuali(SubcQualifiDO subcQuali) {
         return EqlUtils.getInstance("druid").select("getSubcQuali").params(subcQuali).execute();
     }
+
+    /**
+     * ----------------------------Junit测试--------------------------------------
+     */
+
+    @Test
+    public void testlistSubcuQuali(){
+        List<SubcQualifiDO> subcQualifiDOListTest=EqlUtils.getInstance("druid").select("listSubcuQuali").execute();
+        System.out.println(subcQualifiDOListTest.get(1));
+        System.out.println(subcQualifiDOListTest.get(2));
+        System.out.println(subcQualifiDOListTest.size());
+    }
+
+    @Test
+    public void testsaveSubcuQuali(){
+        SubcQualifiDO subcQualifiDOTest=new SubcQualifiDO();
+        for (int i=1;i<=10;i++)
+        {
+            subcQualifiDOTest.setSubcQualifiUuid("wzy"+i);
+            subcQualifiDOTest.setSubcUuid("sub"+i);
+            subcQualifiDOTest.setBusinessLine("化工"+i+"号线");
+            subcQualifiDOTest.setOrgUuid("wylzsyforever");
+            subcQualifiDOTest.setAppSysUuid("wylzsyeveryday");
+            int count=EqlUtils.getInstance("druid").insert("saveSubcuQuali").params(subcQualifiDOTest).execute();
+            System.out.println(count);
+        }
+    }
+
+    @Test
+    public void testRemoveSubcuQuali(){
+        String uuid="wzy1,wzy2,wzy10";
+        String[] ids=uuid.split(",");
+        Map<String,Object> data=new HashMap<>();
+        data.put("ids",ids);
+        int count=EqlUtils.getInstance("druid").delete("removeSubcuQuali").params(data).execute();
+        System.out.println(count);
+    }
+
+    @Test
+    public void testUpdateSubcuQuali(){
+        SubcQualifiDO subcQualifiDOTest=new SubcQualifiDO();
+        subcQualifiDOTest.setSubcQualifiUuid("wzy5");
+        int count=EqlUtils.getInstance("druid").update("updateSubcuQuali").params(subcQualifiDOTest).execute();
+        System.out.println(count);
+    }
+
+    @Test
+    public void testGetSubcQuali(){
+        SubcQualifiDO subcQualifiDOGetTest=new SubcQualifiDO();
+        subcQualifiDOGetTest.setSubcQualifiUuid("wzy9");
+        SubcQualifiDO subcQualifiDOData=EqlUtils.getInstance("druid").selectFirst("getSubcQuali").params(subcQualifiDOGetTest).returnType(SubcQualifiDO.class).execute();
+        System.out.println(subcQualifiDOData);
+    }
+
 }
--- middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/dao/Impl/SubcontractorDaoImpl.java
+++ middle-module/middle-customer/middle-customer-provider/src/main/java/com/ccicnavi/bims/customer/dao/Impl/SubcontractorDaoImpl.java
@@ -3,6 +3,7 @@ package com.ccicnavi.bims.customer.dao.Impl;
 import com.ccicnavi.bims.customer.dao.SubcontractorDao;
 import com.ccicnavi.bims.customer.pojo.SubcontractorDO;
 import com.ccicnavi.bims.customer.util.EqlUtils;
+import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -37,4 +38,61 @@ public class SubcontractorDaoImpl implements SubcontractorDao {
     public SubcontractorDO getSubcontractor(SubcontractorDO subcontractor) {
         return EqlUtils.getInstance("druid").select("getSubcontractor").params(subcontractor).execute();
     }
+
+    /**
+     * ————————————Junit测试————————————————————————
+     */
+
+    @Test
+    public void testListSubcontractor() {
+        List<SubcontractorDO> subcontractorDOList = EqlUtils.getInstance("druid").select("listSubcontractor").returnType(SubcontractorDO.class).execute();
+        System.out.println(subcontractorDOList);
+    }
+
+    @Test
+    public void testSaveSubcontractor() {
+        for (int i = 1; i <= 10; i++) {
+            SubcontractorDO SubcontractorDO = new SubcontractorDO();
+            SubcontractorDO.setSubcontractorUuid("asd" + i);
+            SubcontractorDO.setBusinessLine("类型" + i);
+            SubcontractorDO.setOrgUuid("另一个"+i);
+            SubcontractorDO.setAppSysUuid("DeptUuid"+i);
+            SubcontractorDO.setCreatedUuid("cud"+i);
+            SubcontractorDO.setCreatedName("徐"+i);
+            int count = EqlUtils.getInstance("druid").insert("saveSubcontractor").params(SubcontractorDO).execute();
+            System.out.println("count-----" + count);
+        }
+    }
+
+    @Test
+    public void testUpdateSubcontractor() {
+        SubcontractorDO SubcontractorDO = new SubcontractorDO();
+        SubcontractorDO.setSubcontractorUuid("asd4");
+        SubcontractorDO.setCreatedName("徐4被修改了~");
+        int count = EqlUtils.getInstance("druid").update("updateSubcontractor").params(SubcontractorDO).execute();
+        System.out.println(count);
+    }
+
+
+    @Test
+    public void testRemoveSubcontractor() {
+        String uuids = "asd1,asd10,asd2";
+        String[] ids = uuids.split(",");
+        Map<String, Object> data = new HashMap<String, Object>();
+        data.put("ids", ids);
+        int count = EqlUtils.getInstance("druid").update("removeSubcontractor").params(data).execute();
+        System.out.println(count);
+    }
+
+    @Test
+    public void testGetSubcontractor() {
+        SubcontractorDO SubcontractorDO = new SubcontractorDO();
+        SubcontractorDO.setSubcontractorUuid("asd9");
+        SubcontractorDO getSubcontractor = EqlUtils.getInstance("druid").selectFirst("getSubcontractor").params(SubcontractorDO).returnType(SubcontractorDO.class).execute();
+        System.out.println(getSubcontractor);
+    }
+
 }
+
+    
+
