e9eb1e0692be1c64e8ef82cd68086e55d8dcf00a
liujiale <lyl_beat@163.com>
2018-11-16 11:21:57 +0800

    新增注释

--- zoo-shiba/shiba-api/src/main/java/com/ccicnavi/bims/shiba/api/SetTemplate.java
+++ zoo-shiba/shiba-api/src/main/java/com/ccicnavi/bims/shiba/api/SetTemplate.java
@@ -4,11 +4,35 @@ import java.util.Set;
 
 public interface SetTemplate {
 
+    /**
+     * 添加set类型的缓存
+     *
+     * @param key
+     * @param value
+     */
     void add(Object key, Set<Object> value);
 
+    /**
+     * 根据key查询缓存
+     *
+     * @param key
+     * @return
+     */
     Set members(Object key);
 
+    /**
+     * 根据key删除缓存
+     *
+     * @param key
+     * @return
+     */
     Boolean delete(Object key);
 
+    /**
+     * 查询缓存中value值是否存在
+     *
+     * @param key
+     * @return
+     */
     Boolean hasKey(Object key);
 }
