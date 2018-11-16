4ecae4f27567c3a5205271f085e9618391488440
mengzijie <mengzijie@yuanh.net>
2018-11-16 21:49:03 +0800

    Mod 定义默认值为null

--- middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/service/LogDetailServiceImpl.java
+++ middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/service/LogDetailServiceImpl.java
@@ -48,7 +48,7 @@ public class LogDetailServiceImpl implements LogDetailService {
      */
     @Override
     public Integer insertLogDetail(LogDTO logDTO) {
-        int logDetail = 0;
+        Integer logDetail = null;
         try {
             logDetail = LogDetailDao.insertLogDetail(logDTO);
         } catch (Exception e) {
@@ -66,7 +66,7 @@ public class LogDetailServiceImpl implements LogDetailService {
      */
     @Override
     public Integer updateLogDetail(LogDTO logDTO) {
-        int logDetail = 0;
+        Integer logDetail = null;
         try {
             logDetail = LogDetailDao.updateLogDetail(logDTO);
         } catch (Exception e) {
@@ -84,7 +84,7 @@ public class LogDetailServiceImpl implements LogDetailService {
      */
     @Override
     public Integer deleteLogDetail(LogDTO logDTO) {
-        int logDetail = 0;
+        Integer logDetail = null;
         try {
             logDetail = LogDetailDao.deleteLogDetail(logDTO);
         } catch (Exception e) {
