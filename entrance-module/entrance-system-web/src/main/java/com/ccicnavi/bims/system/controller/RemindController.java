30ef621de1892361ab4f51a3174c0323b377063a
zhangqingqing <2067838559@qq.com>
2018-11-16 16:07:09 +0800

    提醒设置api和实现类,dao和实现类已经eql

--- entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/controller/RemindController.java
+++ entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/controller/RemindController.java
@@ -3,8 +3,13 @@ package com.ccicnavi.bims.system.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.ccicnavi.bims.common.ResultCode;
 import com.ccicnavi.bims.common.ResultT;
+import com.ccicnavi.bims.common.service.pojo.PageBean;
+import com.ccicnavi.bims.common.service.pojo.PageParameter;
 import com.ccicnavi.bims.system.pojo.RemindDO;
+import com.ccicnavi.bims.system.pojo.RemindDTO;
 import com.ccicnavi.bims.system.service.api.RemindService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,27 +30,29 @@ import static com.ccicnavi.bims.common.ResultCode.*;
 @RequestMapping("/remind")
 public class RemindController {
 
+    private final static Logger log = LoggerFactory.getLogger(RemindController.class);
     @Reference(timeout = 3000,url = "dubbo://127.0.0.1:20881")
     private RemindService remindServic;
 
     /**
     *@Description: 查询提醒设置
-    *@Param: sysRemind
-    *@return: List<SysRemindDO>
+    *@Param: PageParameter
+    *@return: PageBean<RemindDO>
     *@Author: zqq
     *@date: 2018/11/15
     */
 
     @RequestMapping(value = "/listRemind", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public ResultT listRemind(@RequestBody RemindDO Remind){
+    public ResultT listRemind(@RequestBody PageParameter<RemindDO>  PageParameter){
 
         try {
-            List<RemindDO> list = remindServic.listRemind(Remind);
-            return ResultT.success(list);
+            PageBean<RemindDO> pageBean = remindServic.listRemind(PageParameter);
+            return ResultT.success(pageBean);
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResultT.failure(ResultCode.RENIND_LIST_RENINDLIST);
+            log.error("根据条件查询提醒设置失败", e);
+            return ResultT.failure(ResultCode.LIST_FAILURE);
         }
+
     }
     /**
     *@Description: 新增提醒设置
@@ -62,8 +69,8 @@ public class RemindController {
             Integer num = remindServic.insertRemind(Remind);
             return ResultT.success();
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResultT.failure(ResultCode.RENIND_LIST_RENINDINSERT);
+            log.error("新增提醒设置失败", e);
+            return ResultT.failure(ResultCode.ADD_FAILURE);
         }
     }
     /**
@@ -80,8 +87,8 @@ public class RemindController {
             Integer num = remindServic.updateRemind(Remind);
             return ResultT.success();
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResultT.failure(ResultCode.RENIND_LIST_RENINDUPDATE);
+            log.error("修改提醒设置失败", e);
+            return ResultT.failure(ResultCode.UPDATE_FAILURE);
         }
     }
     /**
@@ -98,8 +105,8 @@ public class RemindController {
             Integer num = remindServic.deleteRemind(Remind);
             return ResultT.success();
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResultT.failure(ResultCode.RENIND_LIST_RENINDDELETE);
+            log.error("删除提醒设置失败", e);
+            return ResultT.failure(ResultCode.DELETE_FAILURE);
         }
     }
     /**
@@ -116,8 +123,26 @@ public class RemindController {
             RemindDO remindDO= remindServic.getRemind(remind);
             return ResultT.success(remindDO);
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResultT.failure(ResultCode.RENIND_LIST_RENINDGET);
+            log.error("根据主键查询提醒设置失败", e);
+            return ResultT.failure(ResultCode.GET_FAILURE);
+        }
+    }
+    /**
+    *@Description: 连表查询设置提醒
+    *@Param: PageParameter
+    *@return: PageBean<RemindDTO>
+    *@Author: zqq
+    *@date: 2018/11/16
+    */
+
+    @RequestMapping(value = "/lisrRemindOrg", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public ResultT lisrRemindOrg(@RequestBody PageParameter<RemindDO>  PageParameter){
+        try {
+            PageBean<RemindDTO> pageBean = remindServic.listRemindOrg(PageParameter);
+            return ResultT.success(pageBean);
+        } catch (Exception e) {
+            log.error("查询提醒设置失败", e);
+            return ResultT.failure(ResultCode.LIST_FAILURE);
         }
     }
 }
