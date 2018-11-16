78ca7f2fc76b68fc06553c240d5db5f9fad3602e
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-16 21:46:57 +0800

    Mod 启动类修改修改

--- entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/EntranceCustomerApplicaiton.java
+++ entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/EntranceCustomerApplicaiton.java
@@ -20,17 +20,4 @@ public class EntranceCustomerApplicaiton {
         SpringApplication.run(EntranceCustomerApplicaiton.class,args);
     }
 
-    @RestController
-    @RequestMapping("/customer")
-    public static class CustomerDemoController {
-
-
-        @Reference(version = "1.0.0",timeout = 1000,url = "dubbo://127.0.0.1:20883")
-        DemoService demoService;
-
-        @RequestMapping("/sayHello")
-        public String sayHello(String name){
-            return demoService.sayHello(name);
-        }
-    }
 }
