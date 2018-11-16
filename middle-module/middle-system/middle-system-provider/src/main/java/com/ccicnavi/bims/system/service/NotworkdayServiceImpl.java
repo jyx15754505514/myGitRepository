0a73ade3dca1d4cbd5894c37f55462ecd41a9236
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-16 10:56:59 +0800

    Mod 非工作日、角色增删改查方法修改

--- middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/service/NotworkdayServiceImpl.java
+++ middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/service/NotworkdayServiceImpl.java
@@ -2,17 +2,9 @@ package com.ccicnavi.bims.system.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.ccicnavi.bims.system.dao.NotworkdayDao;
-import com.ccicnavi.bims.system.dao.RoleDao;
-import com.ccicnavi.bims.system.dao.impl.NotworkdayDaoImpl;
-import com.ccicnavi.bims.system.dao.impl.RoleDaoImpl;
 import com.ccicnavi.bims.system.pojo.NotworkdayDO;
-import com.ccicnavi.bims.system.pojo.RoleDO;
 import com.ccicnavi.bims.system.service.api.NotworkdayService;
-import org.n3r.eql.Eql;
-import org.n3r.eql.EqlTran;
-import org.n3r.eql.util.Closes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -24,14 +16,11 @@ import java.util.List;
  * @create: 2018-11-14 23:33
  **/
 @Service
+@Slf4j
 public class NotworkdayServiceImpl implements NotworkdayService {
-
-    private final static Logger log = LoggerFactory.getLogger(NotworkdayDaoImpl.class);
 
     @Autowired
     private NotworkdayDao notworkdayDao;
-    @Autowired
-    private RoleDao roleDao;
 
     @Override
     public List<NotworkdayDO> listNotworkday(NotworkdayDO notworkday) {
@@ -45,7 +34,6 @@ public class NotworkdayServiceImpl implements NotworkdayService {
 
     @Override
     public Integer insertNotworkday(NotworkdayDO notworkday) {
-        EqlTran tran = new Eql("DEFAULT").newTran();
         try {
             return notworkdayDao.insertNotworkday(notworkday);
         } catch (Exception e) {
@@ -84,33 +72,4 @@ public class NotworkdayServiceImpl implements NotworkdayService {
         }
     }
 
-
-
-    public Integer test(NotworkdayDO notworkday) {
-        EqlTran tran = new Eql("DEFAULT").newTran();
-        NotworkdayDaoImpl dao = new NotworkdayDaoImpl();
-        RoleDaoImpl roled = new RoleDaoImpl();
-        try {
-            tran.start();
-            Integer test = dao.test(notworkday, tran);
-            RoleDO role = new RoleDO();
-            role.setAppSysUuid("Test1");
-            //role.setBusinessLine("Test1");
-            role.setOrgUuid("1Test11");
-            role.setRoleUuid("1Test1111");
-            role.setRoleCode("Test1111");
-            role.setIsSystem("Y");
-            role.setRoleName("Testrole");
-            role.setIsEnabled("Y");
-            roled.test(role, tran);;
-            tran.commit();
-            return test;
-        } catch (Exception e) {
-            log.error("create service number bind info error: ", e);
-            tran.rollback();
-            return null;
-        } finally {
-            Closes.closeQuietly(tran);
-        }
-    }
 }
