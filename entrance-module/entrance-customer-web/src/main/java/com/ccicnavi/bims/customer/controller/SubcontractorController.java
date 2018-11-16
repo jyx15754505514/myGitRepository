b2ba02ab5f8a1411a7e3c07c6d9937a5a8dcddf2
StaticAnn <37128990+StaticAnn@users.noreply.github.com>
2018-11-16 21:56:55 +0800

    增加了分包方信息(crm_subcontractor)Controller层代码

--- entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/controller/SubcontractorController.java
+++ entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/controller/SubcontractorController.java
@@ -1,6 +1,7 @@
 package com.ccicnavi.bims.customer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.ccicnavi.bims.common.ResultCode;
 import com.ccicnavi.bims.common.ResultT;
 import com.ccicnavi.bims.customer.pojo.SubcontractorDO;
 import com.ccicnavi.bims.customer.api.SubcontractorService;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 /**
  * @program: bims-backend
  * @description: 分包方信息controller
@@ -22,7 +22,7 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/Subcontractor")
+@RequestMapping("/subcontractor")
 public class SubcontractorController {
 
     private final static Logger log= LoggerFactory.getLogger(SubcontractorController.class);
@@ -38,16 +38,14 @@ public class SubcontractorController {
      */
     @RequestMapping(value = "/listSubcontractor",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT listSubcontractor(@RequestBody SubcontractorDO subcontractorDO){
-        ResultT resultT=new ResultT();
         List<SubcontractorDO> subcontractorDOS=null;
         try {
             subcontractorDOS=subcontractorService.listSubcontractor(subcontractorDO);
+            return ResultT.success(subcontractorDOS);
         } catch (Exception e) {
             log.error("查询分包方信息失败",e);
-            e.printStackTrace();
+            return ResultT.failure(ResultCode.LIST_FAILURE);
         }
-        resultT.success(subcontractorDOS);
-        return resultT;
     }
 
     /**
@@ -58,15 +56,68 @@ public class SubcontractorController {
      */
     @RequestMapping(value = "/saveSubcontractor",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT saveSubcontractor(@RequestBody SubcontractorDO subcontractorDO){
-        ResultT resultT=new ResultT();
         int count=0;
         try {
             count=subcontractorService.saveSubcontractor(subcontractorDO);
+            return ResultT.success(count);
         } catch (Exception e) {
             log.error("新增分包方信息失败",e);
-            e.printStackTrace();
+            return ResultT.failure(ResultCode.ADD_FAILURE);
         }
-        return resultT;
+    }
+
+    /**
+     * @author: WangYingLing
+     * @description: 修改分包方信息
+     * @param: java.lang.String
+     * @return: com.ccicnavi.bims.common.ResultT
+     */
+    @RequestMapping(value = "/updateSubcontractor",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public ResultT updateSubcontractor(@RequestBody SubcontractorDO subcontractorDO){
+        int count=0;
+        try {
+            count=subcontractorService.updateSubcontractor(subcontractorDO);
+            return ResultT.success(count);
+        } catch (Exception e) {
+            log.error("修改分包方信息失败",e);
+            return ResultT.failure(ResultCode.UPDATE_FAILURE);
+        }
+    }
+
+    /**
+     * @author: WangYingLing
+     * @description: 删除分包方信息
+     * @param: subcontractorDO
+     * @return: com.ccicnavi.bims.common.ResultT
+     */
+    @RequestMapping(value = "/removeSubcontractor",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public ResultT removeSubcontractor(@RequestBody String subcontractorUuid){
+        int count=0;
+        try {
+            count=subcontractorService.removeSubcontractor(subcontractorUuid);
+            return ResultT.success(count);
+        } catch (Exception e) {
+            log.error("删除分包方信息失败",e);
+            return ResultT.failure(ResultCode.DELETE_FAILURE);
+        }
+    }
+
+    /**
+     * @author: WangYingLing
+     * @description: 查询分包方信息（根据主键获取）
+     * @param: subcontractorDO
+     * @return: com.ccicnavi.bims.common.ResultT
+     */
+    @RequestMapping(value = "/getSubcontractor",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
+    public ResultT getSubcontractor(@RequestBody SubcontractorDO subcontractorDORequstBody){
+        SubcontractorDO subcontractorDO=null;
+        try {
+            subcontractorDO=subcontractorService.getSubcontractor(subcontractorDORequstBody);
+            return ResultT.success(subcontractorDO);
+        } catch (Exception e) {
+            log.error("查询分包方信息失败",e);
+            return ResultT.failure(ResultCode.GET_FAILURE);
+        }
     }
 
 }
