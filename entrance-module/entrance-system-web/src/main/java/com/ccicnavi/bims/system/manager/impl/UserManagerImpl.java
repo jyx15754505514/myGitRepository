791d657c5115c09fc2199f1e878c8506c60d30d4
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-19 23:46:18 +0800

    Mod 用户登录校验

--- entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/manager/impl/UserManagerImpl.java
+++ entrance-module/entrance-system-web/src/main/java/com/ccicnavi/bims/system/manager/impl/UserManagerImpl.java
@@ -1,13 +1,23 @@
 package com.ccicnavi.bims.system.manager.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.ccicnavi.bims.breeder.api.PasswdService;
 import com.ccicnavi.bims.common.ResultCode;
 import com.ccicnavi.bims.common.ResultT;
+import com.ccicnavi.bims.shiba.api.HashTemplate;
 import com.ccicnavi.bims.system.manager.UserManager;
+import com.ccicnavi.bims.system.pojo.DepartmentDO;
+import com.ccicnavi.bims.system.pojo.MenuDO;
 import com.ccicnavi.bims.system.pojo.RoleDO;
 import com.ccicnavi.bims.system.pojo.UserDO;
+import com.ccicnavi.bims.system.service.api.DepartmentService;
+import com.ccicnavi.bims.system.service.api.MenuService;
+import com.ccicnavi.bims.system.service.api.RoleService;
 import com.ccicnavi.bims.system.service.api.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @ClassName: UserManagerImpl
@@ -20,6 +30,21 @@ public class UserManagerImpl implements UserManager {
 
     @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20881")
     private UserService userService;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20881")
+    private RoleService roleService;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20881")
+    private DepartmentService deptService;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20881")
+    private MenuService menuService;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20888")
+    private HashTemplate hashTemplate;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20880")
+    private PasswdService passwdService;
     
     /*
     * 用户登录
@@ -36,16 +61,54 @@ public class UserManagerImpl implements UserManager {
             if("N".equals(user.getIsEnabled())) {
                 return ResultT.failure(ResultCode.USER_ACCOUNT_FORBIDDEN);
             }
-            //查角色
-            //查部门
-            //查权限
-            //查菜单
-            //查产品线
+            String password = userDO.getCurrentPassword();
+            //获取盐值
+            String salt = "";
+            boolean verify = passwdService.verify(user.getCurrentPassword(), password, salt);
+            //密码错误
+            if(!verify) {
+                Integer failedLogins = userDO.getFailedLogins();
+                failedLogins += 1;
+                userDO.setFailedLogins(failedLogins);
+                //失败5次处理逻辑
+                if(failedLogins >= 5) {
 
-
+                }
+                userService.updateUser(userDO);
+                return ResultT.failure(ResultCode.USER_LOGIN_ERROR);
+            }
+            getUserBaseData(userDO, user);
+            //设置过期时间
+            //响应结果
+            return ResultT.success(user);
         }else {
+            //账号不存在
             return ResultT.failure(ResultCode.USER_LOGIN_ERROR);
         }
-        return null;
+    }
+
+    /*
+    * 获取用户的基础信息，角色，部门，权限菜单
+    * @Author zhaotao
+    * @Date  2018/11/19 15:11
+    * @Param [userDO, user]
+    * @return void
+    **/
+    private void getUserBaseData(UserDO userDO, UserDO user) {
+        //创建jsessionID
+        String jsessionID = "";
+        //查角色
+        List<RoleDO> roleList = roleService.listRoleByUser(userDO);
+        //查部门
+        List<DepartmentDO> deptList = deptService.listDeptByUser(userDO);
+        //查权限
+        List<MenuDO> menuList = menuService.listMenuByUser(userDO);
+        //查菜单,根据角色查询菜单List，去除重复
+        //查产品线
+        user.setDeptList(deptList);
+        user.setRoleList(roleList);
+        user.setMenuList(menuList);
+        //保存到Redis中
+        hashTemplate.put(jsessionID, user.getUserUuid(), user);
     }
 }
