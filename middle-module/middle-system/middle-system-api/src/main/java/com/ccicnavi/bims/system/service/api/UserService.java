02a4b286c1fdc3aada90c16d9dfd5669062d71fd
zhaotao@yuanh.net <zhaotao@yuanh.net>
2018-11-19 11:04:36 +0800

    Add 添加登录用户增删改查方法

---
+++ middle-module/middle-system/middle-system-api/src/main/java/com/ccicnavi/bims/system/service/api/UserService.java
@@ -0,0 +1,48 @@
+package com.ccicnavi.bims.system.service.api;
+
+import com.ccicnavi.bims.common.ResultT;
+import com.ccicnavi.bims.common.service.pojo.PageParameter;
+import com.ccicnavi.bims.system.pojo.UserDO;
+
+/**
+ *@program: bims-backend
+ *@description: 登录用户的api接口
+ *@author: zhangxingbiao
+ *@create: 2018-11-15 14:45
+ */
+public interface UserService {
+    /**
+     * 查询登录用户
+     * @param pageParameter
+     * @return List<UserDO>
+     */
+    public ResultT listUser(PageParameter<UserDO> pageParameter);
+
+    /**
+     * 新增登录用户
+     * @param UserDO
+     * @return Integer
+     */
+    public Integer insertUser(UserDO UserDO);
+
+    /**
+     * 更改登录用户
+     * @param UserDO
+     * @return Integer
+     */
+    public Integer updateUser(UserDO UserDO);
+
+    /**
+     * 删除登录用户
+     * @param UserDO
+     * @return Integer
+     */
+    public Integer deleteUser(UserDO UserDO);
+
+    /**
+     * 根据主键获取单个登录用户信息
+     * @param UserDO
+     * @return UserDO
+     */
+    public  UserDO getUser(UserDO UserDO);
+}
