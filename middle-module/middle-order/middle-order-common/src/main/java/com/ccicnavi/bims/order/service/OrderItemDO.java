d80a9ea9ca3a19695d6e7c333a20803164ee864b
PanYida <panyida@yuanh.net>
2018-11-19 15:55:38 +0800

    Add 添加订单主信息、委托服务项目、委托单运输类型DO Mod 修改middle-order-common下pom文件，引入依赖

---
+++ middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/service/OrderItemDO.java
@@ -0,0 +1,48 @@
+package com.ccicnavi.bims.order.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @program: bims-backend
+ * @description: 委托服务项目DO
+ * @author: panyida
+ * @create: 2018-11-19 15:39
+ **/
+@Getter
+@Setter
+@ToString
+public class OrderItemDO implements Serializable {
+    private String orderItemUuid;
+
+    private String minItemName;
+
+    private String minItemUuid;
+
+    private String minItemType;
+
+    private Integer receivableAmount;
+
+    private Integer refAmount;
+
+    private String refWorkhour;
+
+    private String isSubpkg;
+
+    private String isSubpkgMgr;
+
+    private String acceptOrgUuid;
+
+    private String acceptOrgName;
+
+    private String useStdUuid;
+
+    private String useStdName;
+
+    private List<String> orderItemUuidList;
+
+}
