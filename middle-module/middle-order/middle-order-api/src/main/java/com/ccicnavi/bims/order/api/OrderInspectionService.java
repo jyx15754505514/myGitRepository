ea473c5419f6b8a4d15de86a587730588130c6e9
mengzijie <mengzijie@yuanh.net>
2018-11-20 21:30:08 +0800

    add 委托单运输单命名更改

---
+++ middle-module/middle-order/middle-order-api/src/main/java/com/ccicnavi/bims/order/api/OrderInspectionService.java
@@ -0,0 +1,16 @@
+package com.ccicnavi.bims.order.api;
+
+import com.ccicnavi.bims.order.pojo.OrderInfoDTO;
+/* *
+ * @Author MengZiJie
+ * @Description 委托单运输方式
+ * @Date 16:01 2018/11/19
+ */
+public interface OrderInspectionService {
+
+    /**新增运输方式*/
+    Integer insertOrderShipment(OrderInfoDTO orderInfoDTO);
+    /**更新运输方式*/
+    Integer updateOrderShipment(OrderInfoDTO orderInfoDTO);
+
+}
