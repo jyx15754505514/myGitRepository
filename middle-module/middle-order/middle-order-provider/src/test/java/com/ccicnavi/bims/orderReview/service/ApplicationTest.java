e49692b94ccd0921e2a45590b6b840cfef5aace1
fandongsheng@yuanh.net <>
2018-11-20 16:16:35 +0800

    update   测试类修改

--- middle-module/middle-order/middle-order-provider/src/test/java/com/ccicnavi/bims/orderReview/service/ApplicationTest.java
+++ middle-module/middle-order/middle-order-provider/src/test/java/com/ccicnavi/bims/orderReview/service/ApplicationTest.java
@@ -1,42 +1,16 @@
 package com.ccicnavi.bims.orderReview.service;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSONObject;
 import com.ccicnavi.bims.common.service.pojo.PageBean;
 import com.ccicnavi.bims.common.service.pojo.PageParameter;
-import com.ccicnavi.bims.order.api.OrderReviewService;
 import com.ccicnavi.bims.order.dao.impl.OrderReviewDaoImpl;
 import com.ccicnavi.bims.order.pojo.OrderReviewDO;
-import com.ccicnavi.bims.order.service.OrderReviewImpl;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringBootTest(classes = ApplicationTest.class)
 public class ApplicationTest {
-
-    @Reference(timeout = 1000000,url = "dubbo://127.0.0.1:20886")
-    OrderReviewService orderReviewService;
-    @Test
-    public void listOrderReview() {
-        PageParameter<OrderReviewDO> pageParameter = new PageParameter<>();
-        pageParameter.startPage = 1;
-        pageParameter.pageRows = 2;
-        OrderReviewDO orderReviewDO = new OrderReviewDO();
-        orderReviewDO.setIsPass("Y");
-        pageParameter.setParameter(orderReviewDO);
-
-        String rpageParameterString = JSONObject.toJSONString(pageParameter);
-        PageBean<OrderReviewDO> orderReviewDOPageBean = orderReviewService.testPage(pageParameter);
-        String orderReviewString = JSONObject.toJSONString(orderReviewDOPageBean);
-
-        System.out.println(orderReviewString);
-
-    }
     @Test
     public void insertOrderReview() {
         try {
