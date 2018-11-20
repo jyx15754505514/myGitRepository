e4f20e1767cf08b35c40ceadcb2e80a38441ce8b
fandongsheng@yuanh.net <>
2018-11-20 14:39:00 +0800

    add   添加测试

--- middle-module/middle-order/middle-order-provider/src/test/java/com/ccicnavi/bims/orderReview/service/ApplicationTest.java
+++ middle-module/middle-order/middle-order-provider/src/test/java/com/ccicnavi/bims/orderReview/service/ApplicationTest.java
@@ -1,12 +1,11 @@
-package com.ccicnavi.bims.product.service;
+package com.ccicnavi.bims.orderReview.service;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSONObject;
 import com.ccicnavi.bims.common.service.pojo.PageBean;
 import com.ccicnavi.bims.common.service.pojo.PageParameter;
-
-import com.ccicnavi.bims.system.pojo.RoleDO;
-import com.ccicnavi.bims.system.service.api.PageDemoService;
+import com.ccicnavi.bims.order.api.OrderReviewService;
+import com.ccicnavi.bims.order.pojo.OrderReviewDO;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,21 +18,21 @@ import static org.junit.Assert.*;
 public class ApplicationTest {
 
     @Reference(timeout = 1000000,url = "dubbo://127.0.0.1:20881")
-    PageDemoService pageDemoService;
+    OrderReviewService orderReviewService;
     @Test
-    public void main() {
-        PageParameter<RoleDO> pageParameter = new PageParameter<>();
+    public void insertOrderReview() {
+        PageParameter<OrderReviewDO> pageParameter = new PageParameter<>();
         pageParameter.startPage = 1;
         pageParameter.pageRows = 2;
-        RoleDO roleDO = new RoleDO();
-        roleDO.setRoleCode("2");
-        pageParameter.setParameter(roleDO);
+        OrderReviewDO orderReviewDO = new OrderReviewDO();
+        orderReviewDO.setIsPass("Y");
+         pageParameter.setParameter(orderReviewDO);
 
         String rpageParameterString = JSONObject.toJSONString(pageParameter);
-        PageBean<RoleDO> roleDOPageBean = pageDemoService.testPage(pageParameter);
-        String roleString = JSONObject.toJSONString(roleDOPageBean);
+        PageBean<OrderReviewDO> orderReviewDOPageBean = orderReviewService.testPage(pageParameter);
+        String orderReviewString = JSONObject.toJSONString(orderReviewDOPageBean);
 
-        System.out.println(rpageParameterString);
-        System.out.println(roleString);
+        System.out.println(orderReviewString);
+
     }
 }
