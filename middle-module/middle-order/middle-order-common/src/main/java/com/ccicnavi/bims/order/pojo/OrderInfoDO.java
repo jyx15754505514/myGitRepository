25302b8757cde3299dea21206ead95f4149ddb31
mengzijie <mengzijie@yuanh.net>
2018-11-20 22:24:21 +0800

    mod 委托单去除无用包

--- middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDO.java
+++ middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDO.java
@@ -1,13 +1,10 @@
 package com.ccicnavi.bims.order.pojo;
 
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 /**
  * @program: bims-backend
