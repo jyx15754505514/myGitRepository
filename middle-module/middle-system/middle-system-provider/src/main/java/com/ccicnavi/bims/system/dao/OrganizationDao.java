f247a628b23df4720b9e3f651abb5aa68e179d99
zhangpengwei@yuanh.net <zhangpengwei@yuanh.net>
2018-11-23 00:27:53 +0800

    部门+组织机构 1.方法修改

--- middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/dao/OrganizationDao.java
+++ middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/dao/OrganizationDao.java
@@ -3,43 +3,74 @@ package com.ccicnavi.bims.system.dao;
 import com.ccicnavi.bims.common.service.pojo.PageBean;
 import com.ccicnavi.bims.common.service.pojo.PageParameter;
 import com.ccicnavi.bims.system.pojo.OrganizationDO;
+import com.ccicnavi.bims.system.pojo.OrganizationDTO;
+import com.ccicnavi.bims.system.pojo.UserDO;
+import com.ccicnavi.bims.system.pojo.UserDTO;
 
 import java.util.List;
 
+/**
+ * @program: bims-backend
+ * @description: 组织机构信息
+ * @author: zhangpengwei
+ * @create: 2018-11-15 14:45
+ */
 public interface OrganizationDao {
 
     /**
      * 查询职称信息
-     * @param organizationDO
+     * @param pageParameter
      * @return
      */
-    public PageBean<OrganizationDO> listOrganization(PageParameter<OrganizationDO> pageParameter) throws Exception;
+    public PageBean<OrganizationDTO> listOrganization(PageParameter<OrganizationDTO> pageParameter) throws Exception;
 
     /**
      * 根据主键查询职称信息
-     * @param organizationDO
+     * @param organizationDTO
      * @return
      */
-    public OrganizationDO getOrganization(OrganizationDO organizationDO) throws Exception;
+    public OrganizationDTO getOrganization(OrganizationDTO organizationDTO) throws Exception;
 
     /**
      * 新增职称信息
-     * @param organizationDO
+     * @param organizationDTO
      * @return
      */
-    public Integer insertOrganization(OrganizationDO organizationDO) throws Exception;
+    public Integer insertOrganization(OrganizationDTO organizationDTO) throws Exception;
 
     /**
-     * 修改职称信息
-     * @param organizationDO
-     * @return
+     *@Description: 修改职称信息
+     *@Param: [organizationDTO]
+     *@return: java.lang.Integer
+     *@Author: zhangpengwei
+     *@date: 2018/11/23
      */
-    public Integer updateOrganization(OrganizationDO organizationDO) throws Exception;
+    public Integer updateOrganization(OrganizationDTO organizationDTO) throws Exception;
 
     /**
-     * 删除职称信息
-     * @param  organizationDO
-     * @return
+     *@Description: 删除职称信息
+     *@Param: [organizationDTO]
+     *@return: java.lang.Integer
+     *@Author: zhangpengwei
+     *@date: 2018/11/23
      */
-    public Integer deleteOrganization(OrganizationDO organizationDO) throws Exception;
+    public Integer deleteOrganization(OrganizationDTO organizationDTO) throws Exception;
+
+    /**
+     *@Description: 根据用户查询省公司
+     *@Param: [userDTO]
+     *@return: com.ccicnavi.bims.system.pojo.OrganizationDTO
+     *@Author: zhangpengwei
+     *@date: 2018/11/23
+     */
+    public OrganizationDTO getOrgByUser(UserDTO userDTO) throws Exception;
+
+    /**
+     *@Description: 根据当前用户的orgUuid获取所有的当前机构和子机构
+     *@Param: [organizationDTO]
+     *@return: java.util.List<com.ccicnavi.bims.system.pojo.OrganizationDTO>
+     *@Author: zhangpengwei
+     *@date: 2018/11/23
+     */
+    public List<OrganizationDTO> listOrgByOrg(OrganizationDTO organizationDTO) throws Exception;
 }
--- middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/dao/impl/DepartmentDaoImpl.java
+++ middle-module/middle-system/middle-system-provider/src/main/java/com/ccicnavi/bims/system/dao/impl/DepartmentDaoImpl.java
@@ -1,19 +1,15 @@
 package com.ccicnavi.bims.system.dao.impl;
 
 
-import com.ccicnavi.bims.common.service.com.ccicnavi.bims.common.util.EqlUtils;
 import com.ccicnavi.bims.common.service.pojo.PageBean;
 import com.ccicnavi.bims.common.service.pojo.PageParameter;
 import com.ccicnavi.bims.system.dao.DepartmentDao;
-import com.ccicnavi.bims.system.pojo.DepartmentDO;
-import com.ccicnavi.bims.system.pojo.UserDO;
+import com.ccicnavi.bims.system.pojo.DepartmentDTO;
 import com.ccicnavi.bims.system.pojo.UserDTO;
-import com.ccicnavi.bims.system.pojo.UserDeptDO;
 import lombok.extern.slf4j.Slf4j;
 import org.n3r.eql.Eql;
 import org.n3r.eql.EqlPage;
 import org.springframework.stereotype.Service;
-
 
 import java.util.List;
 
@@ -29,17 +25,17 @@ public class DepartmentDaoImpl implements DepartmentDao {
 
     /**
     *@Description: 查询部门信息
-    *@Param: [departmentDO]
-    *@return: List<DepartmentDO>
+    *@Param: [departmentDTO]
+    *@return: List<DepartmentDTO>
     *@Author: zhangxingbiao
     *@date: 2018/11/15
     */
     @Override
-    public PageBean<DepartmentDO> listDepartment(PageParameter<DepartmentDO> pageParameter){
+    public PageBean<DepartmentDTO> listDepartment(PageParameter<DepartmentDTO> pageParameter) throws Exception {
         EqlPage eqlPage = new EqlPage(pageParameter.getStartIndex(), pageParameter.getPageRows());
-        List<DepartmentDO> departmentDOList = new Eql("DEFAULT").select("listDepartment").params(pageParameter.getParameter()).limit(eqlPage).returnType(DepartmentDO.class).execute();
-        if(departmentDOList != null) {
-            return  new PageBean<DepartmentDO>(eqlPage.getTotalRows(),eqlPage.getTotalPages(), eqlPage.getCurrentPage(), eqlPage.getPageRows(), eqlPage.getStartIndex(), departmentDOList);
+        List<DepartmentDTO> departmentDTOList = new Eql("DEFAULT").select("listDepartment").params(pageParameter.getParameter()).limit(eqlPage).returnType(DepartmentDTO.class).execute();
+        if(departmentDTOList != null) {
+            return  new PageBean<DepartmentDTO>(eqlPage.getTotalRows(),eqlPage.getTotalPages(), eqlPage.getCurrentPage(), eqlPage.getPageRows(), eqlPage.getStartIndex(), departmentDTOList);
         }else {
             return null;
         }
@@ -47,63 +43,85 @@ public class DepartmentDaoImpl implements DepartmentDao {
 
     /**
     *@Description: 新增部门信息
-    *@Param: [departmentDO]
+    *@Param: [departmentDTO]
     *@return: Integer
     *@Author: zhangxingbiao
     *@date: 2018/11/15
     */
     @Override
-    public Integer insertDepartment(DepartmentDO departmentDO){
-        return new Eql().insert("insertDepartment").params(departmentDO).returnType(Integer.class).execute();
+    public Integer insertDepartment(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().insert("insertDepartment").params(departmentDTO).returnType(Integer.class).execute();
     }
 
     /**
     *@Description: 更新部门信息
-    *@Param: [departmentDO]
+    *@Param: [departmentDTO]
     *@return: Integer
     *@Author: zhangxingbiao
     *@date: 2018/11/15
     */
     @Override
-    public Integer updateDepartment(DepartmentDO departmentDO){
-        return new Eql().update("updateDepartment").params(departmentDO).returnType(Integer.class).execute();
+    public Integer updateDepartment(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().update("updateDepartment").params(departmentDTO).returnType(Integer.class).execute();
     }
 
     /**
     *@Description: 删除部门信息
-    *@Param: [departmentDO]
+    *@Param: [departmentDTO]
     *@return: Integer
     *@Author: zhangxingbiao
     *@date: 2018/11/15
     */
     @Override
-    public Integer deleteDepartment(DepartmentDO departmentDO){
-        return new Eql().delete("deleteDepartment").params(departmentDO).returnType(Integer.class).execute();
+    public Integer deleteDepartment(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().delete("deleteDepartment").params(departmentDTO).returnType(Integer.class).execute();
 
     }
 
     /**
     *@Description: 根据主键获取部门信息
-    *@Param: [departmentDO]
-    *@return: DepartmentDO
+    *@Param: [departmentDTO]
+    *@return: DepartmentDTO
     *@Author: zhangxingbiao
     *@date: 2018/11/15
     */
     @Override
-    public DepartmentDO getDepartment(DepartmentDO departmentDO){
-        return new Eql().selectFirst("getDepartment").params(departmentDO).returnType(DepartmentDO.class).execute();
+    public DepartmentDTO getDepartment(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().selectFirst("getDepartment").params(departmentDTO).returnType(DepartmentDTO.class).execute();
     }
 
     /**
     *@Description: 根据用户的UUID查询所有的部门信息
     *@Param: [userDO]
-    *@return: java.util.List<com.ccicnavi.bims.system.pojo.UserDeptDO>
+    *@return: java.util.List<com.ccicnavi.bims.system.pojo.DepartmentDTO>
     *@Author: zhangpengwei
     *@date: 2018/11/19
     */
     @Override
-    public List<DepartmentDO> listDeptByUser(UserDTO userDTO) throws Exception {
-        return new Eql().select("listDeptByUser").params(userDTO).returnType(UserDeptDO.class).execute();
+    public List<DepartmentDTO> listDeptByUser(UserDTO userDTO) throws Exception {
+        return new Eql().select("listDeptByUser").params(userDTO).returnType(DepartmentDTO.class).execute();
     }
 
+    /**
+    *@Description: 根据部门类型查询部门信息
+    *@Param: [departmentDTO]
+    *@return: java.util.List<com.ccicnavi.bims.system.pojo.DepartmentDTO>
+    *@Author: zhangpengwei
+    *@date: 2018/11/20
+    */
+    @Override
+    public List<DepartmentDTO> listDeptByDeptType(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().select("listDeptByDeptType").params(departmentDTO).returnType(DepartmentDTO.class).execute();
+    }
+
+    /**
+     *@Description: 根据orgUuid和parentUuid查询部门
+     *@Param: [departmentDTO]
+     *@return: java.util.List<com.ccicnavi.bims.system.pojo.DepartmentDTO>
+     *@Author: zhangpengwei
+     *@date: 2018/11/21
+     */
+    public List<DepartmentDTO> listDept(DepartmentDTO departmentDTO) throws Exception {
+        return new Eql().select("listDept").params(departmentDTO).returnType(DepartmentDTO.class).execute();
+    }
 }
