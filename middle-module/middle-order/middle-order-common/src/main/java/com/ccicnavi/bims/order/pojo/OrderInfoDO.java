12bcf95ca1f33d8f07ca9018b974551658ada7bf
songyateng <songyateng@yuanh.net>
2018-11-22 19:59:46 +0800

    Mod 添加字段备注信息

--- middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDO.java
+++ middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDO.java
@@ -16,121 +16,237 @@ import java.util.Date;
 @Setter
 @ToString
 public class OrderInfoDO implements Serializable {
-
+    /**
+     *委托单主键
+     */
     private String orderUuid;
-
+    /**
+     *产品分类id
+     */
     private String productCategoryUuid;
-
+    /**
+     *委托编号
+     */
     private String orderNo;
-
+    /**
+     *委托单名称
+     */
     private String orderName;
-
+    /**
+     * 委托日期
+     */
     private Date orderDate;
-
+    /**
+     * 客户号
+     */
     private String orderCrmNo;
-
+    /**
+     * 区域id
+     */
     private String orderAreaId;
-
+    /**
+     * 客户id
+     */
     private String crmUuid;
-
+    /**
+     * 客户名称
+     */
     private String crmName;
-
+    /**
+     * 联系人
+     */
     private String crmLinkman;
-
+    /**
+     * 联系人电话
+     */
     private String crmTelphone;
-
+    /**
+     * 联系人邮箱
+     */
     private String crmEmail;
-
+    /**
+     * 合同编号
+     */
     private String crmContractNo;
-
+    /**
+     * 紧急联系人
+     */
     private String crmEmerName;
-
+    /**
+     * 紧急联系人电话
+     */
     private String crmEmerTelphone;
-
+    /**
+     * 委托项目名称
+     */
     private String orderItemName;
-
+    /**
+     * 委托项目类型
+     */
     private String orderItemType;
-
+    /**
+     * 项目规格
+     */
     private String orderItemSpec;
-
+    /**
+     * 数量
+     */
     private Integer orderItemQty;
-
+    /**
+     * 数量单位
+     */
     private String orderItemQtyUnit;
-
+    /**
+     * 使用标准id
+     */
     private String useStdUuid;
-
+    /**
+     * 使用标准名称
+     */
     private String useStdName;
-
+    /**
+     * 标准说明
+     */
     private String useStdComment;
-
+    /**
+     * 总费用
+     */
     private Integer totalFee;
-
+    /**
+     * 首次计费日期
+     */
     private Date firstFeeDate;
-
+    /**
+     * 是Y否N内部委托单
+     */
     private String isInternal;
-
+    /**
+     * 委托单来源机构id
+     */
     private String sourceOrgUuid;
-
+    /**
+     * 委托单来源机构名称
+     */
     private String sourceOrgName;
-
+    /**
+     * 计划检验时间
+     */
     private Date planInspectDate;
-
+    /**
+     * 计划检验地
+     */
     private Date planInspectAddr;
-
+    /**
+     * 备注
+     */
     private String comments;
-
+    /**
+     * 币种id
+     */
     private String currencyType;
-
+    /**
+     * 结论方式
+     */
     private String conclusionType;
-
+    /**
+     * 证书语种id
+     */
     private String certLangUuid;
-
+    /**
+     * 证书正本数量
+     */
     private Integer certQty;
-
+    /**
+     * 证书副本数量
+     */
     private Integer certCopyQty;
-
+    /**
+     * 受理日期
+     */
     private Date acceptDate;
-
+    /**
+     * 受理机构id
+     */
     private String acceptOrgUuid;
-
+    /**
+     * 受理机构名称（公司名称）
+     */
     private String acceptOrgName;
-
+    /**
+     * 状态uuid
+     */
     private String statusUuid;
-
+    /**
+     * 状态名称
+     */
     private String statusName;
-
+    /**
+     * 是Y否N分包标记，默认为N
+     */
     private String lblSubpkg;
-
+    /**
+     * 是Y否N执行标记，默认为N
+     */
     private String lblExec;
-
+    /**
+     * 委托方是Y否N具备专业能力，默认为N
+     */
     private String lblCrmCapc;
-
+    /**
+     * 是Y否N需要评审，默认为N
+     */
     private String isReview;
-
+    /**
+     * 删除标识（N正常Y删除）
+     */
     private String isDeleted;
-
+    /**
+     * 创建时间
+     */
     private Date createdTime;
-
+    /**
+     * 创建人id
+     */
     private String createdUuid;
-
+    /**
+     * 创建人
+     */
     private String createdName;
-
+    /**
+     * 最后一次更新时间
+     */
     private Date updatedTime;
-
+    /**
+     * 更新人uuid
+     */
     private String updatedUuid;
-
+    /**
+     * 更新人
+     */
     private String updatedName;
-
+    /**
+     * 产品线id
+     */
     private String prodCatalogUuid;
-
+    /**
+     * 归属机构id
+     */
     private String orgUuid;
-
+    /**
+     * 应用系统id
+     */
     private String appSysUuid;
-
+    /**
+     * 预留字段1
+     */
     private String extend1;
-
+    /**
+     * 预留字段2
+     */
     private String extend2;
-
+    /**
+     * 预留字段3
+     */
     private String extend3;
 
 }
