fc30566f7b252805a3b23bc7d985872057615c08
WangYingLing <37128990+StaticAnn@users.noreply.github.com>
2018-11-23 21:28:55 +0800

    Update 产品线信息，产品类别的分类，检测指标的Eql文件 Update 产品线信息，产品线与组织机构关系，产品类别的分类，检测指标DO的@JsonInclude(JsonInclude.Include.NON_NULL)注解添加

--- middle-module/middle-product/middle-product-common/src/main/java/com/ccicnavi/bims/product/pojo/TestItemDO.java
+++ middle-module/middle-product/middle-product-common/src/main/java/com/ccicnavi/bims/product/pojo/TestItemDO.java
@@ -1,5 +1,6 @@
 package com.ccicnavi.bims.product.pojo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,6 +17,7 @@ import java.util.Date;
 @Getter
 @Setter
 @ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TestItemDO implements Serializable {
 
     /**
