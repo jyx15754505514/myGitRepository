0f858651c819081a9c700be17fd3806f24c6dfab
张琦 <zhangqi@yuanh.net>
2018-11-23 18:41:04 +0800

    测试可忽略

--- zoo-breeder/breeder-api/src/main/java/com/ccicnavi/bims/breeder/api/IdWorkerService.java
+++ zoo-breeder/breeder-api/src/main/java/com/ccicnavi/bims/breeder/api/IdWorkerService.java
@@ -9,6 +9,24 @@ import java.util.Date;
  * @Description:
  */
 public interface IdWorkerService {
+    /**
+     * 获取唯一ID
+     * @param date 当前时间
+     * @return 唯一ID
+     */
+     String getId(Date date);
 
-    public String getId(Date date);
+
+    /**
+     * 获取业务ID
+     * @param sysUID 机构ID
+     * @param busiId 业务编号
+     * @param step   步长 1000=4,,10000=5
+     * @param cycle  周期  日=D,周=W,月=M,年=Y
+     * @return
+     */
+     String getBusinessNumber(String sysUID,String busiId,String step,String cycle);
+
+
+
 }
