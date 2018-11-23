e588d65eded89236254ac9275147134911e3b1f2
WangYingLing <37128990+StaticAnn@users.noreply.github.com>
2018-11-23 11:21:53 +0800

    Update 分包分资质DO； Add 分包分资质DO注释

--- middle-module/middle-customer/middle-customer-common/src/main/java/com/ccicnavi/bims/customer/pojo/SubcQualifiDO.java
+++ middle-module/middle-customer/middle-customer-common/src/main/java/com/ccicnavi/bims/customer/pojo/SubcQualifiDO.java
@@ -20,38 +20,94 @@ import java.util.Date;
 @ToString
 public class SubcQualifiDO implements Serializable {
 
+    /**
+     * 资质uuid
+     */
     private String subcQualifiUuid;
 
+    /**
+     *所属分包方UUID
+     */
     private String subcUuid;
 
+    /**
+     * 资质范围
+     */
     private String qualifiRange;
 
+    /**
+     * 资质类型(枚举uuid)
+     */
     private String qualifiEnmUuid;
 
+    /**
+     * 资质编码
+     */
+    private String qualifiCode;
+
+    /**
+     * 资质描述
+     */
     private String qualifiDescribe;
 
+    /**
+     * 是否上传文件(Y是N否)'
+     */
     private String isFile;
 
+    /**
+     * 备注
+     */
+    private String comments;
+
+    /**
+     * 删除标识（N正常Y删除）
+     */
     private String isDeleted;
 
+    /**
+     * 创建时间
+     */
     private Date createdTime;
 
+    /**
+     * 创建人id
+     */
     private String createdUuid;
 
+    /**
+     * 创建人
+     */
+    private String createdName;
+
+    /**
+     * 最后一次更新时间
+     */
     private Date updatedTime;
 
+    /**
+     * 更新人uuid
+     */
+    private String updatedUuid;
+
+    /**
+     * 更新人
+     */
     private String updatedName;
 
-    private String businessLine;
-
-    private String orgUuid;
-
-    private String appSysUuid;
-
+    /**
+     * 预留字段1
+     */
     private String extend1;
 
+    /**
+     * 预留字段2
+     */
     private String extend2;
 
+    /**
+     * 预留字段3
+     */
     private String extend3;
 
 }
