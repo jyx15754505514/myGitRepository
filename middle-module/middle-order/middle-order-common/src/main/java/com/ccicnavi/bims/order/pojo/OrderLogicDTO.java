13d13a4c44642041c58eb322f1060bc3c0dfbe16
fandongsheng@yuanh.net <>
2018-11-24 16:14:53 +0800

    MOD  添加字段注释

--- middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderLogicDTO.java
+++ middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderLogicDTO.java
@@ -16,27 +16,51 @@ import java.util.List;
 @Setter
 @ToString
 public class OrderLogicDTO implements Serializable {
-
+    /**
+     * 委托单主键
+     */
     private String orderUuid;
-
+    /**
+     * 委托单主键List
+     */
+    private List<String> orderUuids;
+    /**
+     * 状态
+     */
     private String statusUuid;
-
+    /**
+     * 状态名称
+     */
     private String statusName;
-
+    /**
+     * 是否删除
+     */
     private String isDeleted;
-
+    /**
+     * 更新时间
+     */
     private Date updatedTime;
-
+    /**
+     * 更新人UUID
+     */
     private String updatedUuid;
-
+    /**
+     * 更新人名称
+     */
     private String updatedName;
-
+    /**
+     * 所属产品线
+     */
     private String prodCatalogUuid;
-
+    /**
+     * 所属公司UUID
+     */
     private String orgUuid;
-
+    /**
+     * 所属平台UUID
+     */
     private String appSysUuid;
 
-    List<String> orderUuids;
+
 
 }
