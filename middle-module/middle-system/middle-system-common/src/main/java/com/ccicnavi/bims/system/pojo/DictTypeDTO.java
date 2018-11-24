bc01236288f3eda124ca35b0516778914389fa27
zhangxingbiao@yuanh.net <zhangxingbiao@yuanh.net>
2018-11-24 14:24:00 +0800

    数据字典类型实体类修改

--- middle-module/middle-system/middle-system-common/src/main/java/com/ccicnavi/bims/system/pojo/DictTypeDTO.java
+++ middle-module/middle-system/middle-system-common/src/main/java/com/ccicnavi/bims/system/pojo/DictTypeDTO.java
@@ -21,12 +21,89 @@ import java.util.List;
 @Getter
 @ToString
 public class DictTypeDTO implements Serializable {
-
-
+    /**
+     * 主键uuid
+     */
+    private String dictTypeUuid;
+    /**
+     * 字典类型名称
+     */
+    private String dictTypeName;
+    /**
+     * 字典类型编码
+     */
+    private String dictTypeCode;
+    /**
+     * 排序编号
+     */
+    private String sortNum;
+    /**
+     * 主键uuid
+     */
+    private String dictValueUuid;
+    /**
+     * 字典数值名称
+     */
+    private String dictValueName;
+    /**
+     * 字典数值编码
+     */
+    private String dictValueCode;
+    /**
+     * 父节点uuid
+     */
     private String dictParentUuid;
-
+    /**
+     * 备注
+     */
+    private String comments;
+    /**
+     * 删除标识
+     */
+    private String isDeleted;
+    /**
+     * 创建时间
+     */
+    private Date createdTime;
+    /**
+     * 创建人id
+     */
+    private String createdUuid;
+    /**
+     * 创建人
+     */
+    private String createdName;
+    /**
+     * 最后一次更新时间
+     */
+    private Date updatedTime;
+    /**
+     * 更新人uuid
+     */
+    private String updatedUuid;
+    /**
+     * 更新人
+     */
+    private String updatedName;
+    /**
+     * 归属机构id
+     */
+    private String orgUuid;
+    /**
+     * 应用系统id
+     */
+    private String appSysUuid;
+    /**
+     * 产品线id
+     */
+    private String prodCatalogUuid;
+    /**
+     * 字典数值集合
+     */
     private List<DictValueDO> valueList;
-
+    /**
+     * 字典类型集合
+     */
     private List<String> typeList;
 
 }
