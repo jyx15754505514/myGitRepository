8d74f31f03fd303afcd5972d776711d3104ec48e
张琦 <zhangqi@yuanh.net>
2018-11-26 09:51:55 +0800

    新增服务管理中心

---
+++ zoo-admin/src/test/java/com/ccicnavi/bims/zooadmin/ZooAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ccicnavi.bims.zooadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZooAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
