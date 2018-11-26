1c6051153852f573b79f476760e7d83fbaac649f
Íõ¸ýÏì <1114538320@qq.com>
2018-11-26 23:18:52 +0800

    mod  修改equipmentledgerController

--- entrance-module/entrance-resource-web/src/main/java/com/ccicnavi/bims/resource/controller/EquipmentLedgerController.java
+++ entrance-module/entrance-resource-web/src/main/java/com/ccicnavi/bims/resource/controller/EquipmentLedgerController.java
@@ -1,6 +1,7 @@
 package com.ccicnavi.bims.resource.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.support.Parameter;
 import com.ccicnavi.bims.common.ResultCode;
 import com.ccicnavi.bims.common.ResultT;
 import com.ccicnavi.bims.common.service.pojo.PageBean;
@@ -8,6 +9,8 @@ import com.ccicnavi.bims.common.service.pojo.PageParameter;
 import com.ccicnavi.bims.resource.api.EquipService;
 import com.ccicnavi.bims.resource.pojo.EquipDO;
 import com.ccicnavi.bims.resource.pojo.EquipDTO;
+import com.ccicnavi.bims.system.pojo.RemindDTO;
+import com.ccicnavi.bims.system.service.api.RemindService;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.PortUnreachableException;
@@ -20,6 +23,9 @@ public class EquipmentLedgerController {
 
     @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20882")
     EquipService equipService;
+
+    @Reference(timeout = 30000, url = "dubbo://127.0.0.1:20881")
+    RemindService remindService;
 
     /*
      *@Param: [pageParameter]
@@ -52,7 +58,7 @@ public class EquipmentLedgerController {
         Date testDate = equipDO.getTestDate();
         Date testValidDate = equipDO.getTestValidDate();
         //验证 设备名称 设备检/校日期  设备有效日期 非空判断
-        if (name == null || name.equals("") || testDate == null || testDate.equals("") || testValidDate == null || testValidDate.equals("")) {
+        if (name == null || name.equals("") || testDate == null || testValidDate == null) {
             return ResultT.failure(ResultCode.REQUIRED_FIELDS_IS_NULL);
         }
         try {
@@ -81,7 +87,7 @@ public class EquipmentLedgerController {
         Date testDate = equipDO.getTestDate();
         Date testValidDate = equipDO.getTestValidDate();
         //验证 设备名称 设备检/校日期  设备有效日期  非空判断
-        if (name == null || name.equals("") || testDate == null || testDate.equals("") || testValidDate == null || testValidDate.equals("")) {
+        if (name == null || name.equals("") || testDate == null || testValidDate == null) {
             return ResultT.failure(ResultCode.REQUIRED_FIELDS_IS_NULL);
         }
         try {
@@ -120,17 +126,66 @@ public class EquipmentLedgerController {
     }
 
     /*
-    *@Param: [equipDO]
-    *@description: 根据设备uuid 获取设备详情信息、领用记录、检定记录
-    *@return: com.ccicnavi.bims.common.ResultT
-    *@author: WangGengXiang
-    *@create: 2018/11/24 11:47
-    */
+     *@Param: [equipDO]
+     *@description: 根据设备uuid 获取设备详情信息、领用记录、检定记录
+     *@return: com.ccicnavi.bims.common.ResultT
+     *@author: WangGengXiang
+     *@create: 2018/11/24 11:47
+     */
     @RequestMapping(value = "/editEquipLedger", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public ResultT editEquip(@RequestBody EquipDTO equipDTO) {
         try {
             EquipDTO equipInfolist = equipService.getEquipInfolist(equipDTO);
             return ResultT.success(equipInfolist);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failure(ResultCode.LIST_FAILURE);
+        }
+    }
+
+    /*
+     *@Param: [pageParameter]
+     *@description: 到期提醒查询
+     *@return: com.ccicnavi.bims.common.ResultT
+     *@author: WangGengXiang
+     *@create: 2018/11/26 20:27
+     */
+    @RequestMapping(value = "/listRemindEquip", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public ResultT expireListEquip(@RequestBody PageParameter<EquipDTO> pageParameter) {
+        try {
+            RemindDTO remindDTO = new RemindDTO();
+            //remindDTO.setAppSysUuid("");
+            RemindDTO remindDTO1 = remindService.getRemind(remindDTO);
+            //获取设置的提醒天数
+            pageParameter.getParameter().setExpireDay(Integer.parseInt(remindDTO1.getNum()));
+            PageBean<EquipDO> pageBean = equipService.expireListEquip(pageParameter);
+
+            return ResultT.success(pageBean);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failure(ResultCode.LIST_FAILURE);
+        }
+    }
+
+    /*
+     *@Param: [days]
+     *@description: 更新查询日期
+     *@return: com.ccicnavi.bims.common.ResultT
+     *@author: WangGengXiang
+     *@create: 2018/11/26 21:45
+     */
+    @RequestMapping(value = "/setUpRemindDay", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public ResultT expireListEquip(@RequestBody(required = true) Integer days) {
+        try {
+            RemindDTO remindDTO = new RemindDTO();
+            remindDTO.setNum(days.toString());
+            
+            Integer num = remindService.updateRemind(remindDTO);
+            if (num > 0) {
+                return ResultT.success("更新提醒天数成功");
+            } else {
+                return ResultT.failure(ResultCode.UPDATE_FAILURE);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return ResultT.failure(ResultCode.LIST_FAILURE);
