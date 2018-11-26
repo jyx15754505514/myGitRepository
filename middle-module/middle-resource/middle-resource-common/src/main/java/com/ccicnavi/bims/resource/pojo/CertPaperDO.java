a0183d0d2f18882f8994891d7e9af39637b2213f
heibin <heibin>
2018-11-26 16:03:38 +0800

    证书纸DO注释

--- middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/CertPaperDO.java
+++ middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/CertPaperDO.java
@@ -1,65 +1,118 @@
 package com.ccicnavi.bims.resource.pojo;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
 
 import java.io.Serializable;
 import java.util.Date;
-/*
+/**
  * 证书纸
  * @Date 2018/11/13 17 54
  * @Auther mzj
  */
-@Getter
-@Setter
-@ToString
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CertPaperDO  implements Serializable {
+    /**
+     * 证书纸uuid
+     */
     private String paperUuid;
-
+    /**
+     * 流水起始号
+     */
     private String paperStartNum;
-
+    /**
+     * 流水截止号
+     */
     private String paperEndNum;
-
+    /**
+     * 入库人
+     */
     private String stromPerson;
-
+    /**
+     * 入库时间
+     */
     private Date stromDate;
-
+    /**
+     * 领用单位
+     */
     private String leadUnit;
-
+    /**
+     * 领用部门
+     */
     private String leadDept;
-
+    /**
+     * 证书纸状态
+     */
     private String paperStatus;
-
+    /**
+     * 入库数量
+     */
     private String stromNum;
-
+    /**
+     * 剩余数量
+     */
     private String residualNum;
-
+    /**
+     * 当前号
+     */
     private String currentCode;
-
+    /**
+     * 删除标识（N正常Y删除）
+     */
     private String isDeleted;
-
+    /**
+     * 创建时间
+     */
     private Date createdTime;
-
+    /**
+     * 创建人id
+     */
     private String createdUuid;
-
+    /**
+     * 创建人
+     */
+    private String createdName;
+    /**
+     * 最后一次更新时间
+     */
     private Date updatedTime;
+    /**
+     * 更新人uuid
+     */
+    private String updatedUuid;
 
+    /**
+     * 更新人
+     */
     private String updatedName;
-
-    private String businessLine;
-
+    /**
+     * 归属机构id
+     */
     private String orgUuid;
-
+    /**
+     * 应用系统id
+     */
     private String appSysUuid;
-
+    /**
+     * 预留字段1
+     */
     private String extend1;
-
+    /**
+     * 预留字段2
+     */
     private String extend2;
-
+    /**
+     * 预留字段3
+     */
     private String extend3;
-
+    /**
+     * 备注
+     */
     private String comments;
-
+    /**
+     * 产品线id
+     */
     private String prodCatalogUuid;
 }
