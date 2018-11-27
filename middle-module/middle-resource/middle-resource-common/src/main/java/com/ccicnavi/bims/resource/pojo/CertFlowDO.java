4378642ba5ab21a8663beda3aad7150fe79888fb
heibin <heibin>
2018-11-27 14:54:02 +0800

    CertFlowDO 实现序列化

--- middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/CertFlowDO.java
+++ middle-module/middle-resource/middle-resource-common/src/main/java/com/ccicnavi/bims/resource/pojo/CertFlowDO.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -13,7 +14,7 @@ import java.util.Date;
  */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class CertFlowDO {
+public class CertFlowDO implements Serializable {
     /**
      * 证书流水uuid
      */
