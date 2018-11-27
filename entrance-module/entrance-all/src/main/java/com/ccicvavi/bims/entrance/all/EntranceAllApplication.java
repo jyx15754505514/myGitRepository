103e5dd5680ebad576105ad74155685ca5bac4aa
liujiale <lyl_beat@163.com>
2018-11-27 15:28:41 +0800

    新增启动类

---
+++ entrance-module/entrance-all/src/main/java/com/ccicvavi/bims/entrance/all/EntranceAllApplication.java
@@ -0,0 +1,14 @@
+package com.ccicvavi.bims.entrance.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ccicnavi.bims.*"})
+public class EntranceAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceAllApplication.class, args);
+    }
+}
