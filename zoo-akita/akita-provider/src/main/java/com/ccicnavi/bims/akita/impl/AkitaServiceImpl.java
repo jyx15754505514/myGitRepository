b1f545eda9a4a2e17f500e1ee53e4c5a63b4d584
张琦 <zhangqi@yuanh.net>
2018-11-28 16:06:28 +0800

    Test 请忽略

--- zoo-akita/akita-provider/src/main/java/com/ccicnavi/bims/akita/impl/AkitaServiceImpl.java
+++ zoo-akita/akita-provider/src/main/java/com/ccicnavi/bims/akita/impl/AkitaServiceImpl.java
@@ -8,16 +8,19 @@ import com.ccicnavi.bims.akita.common.pojo.domain.AttaDO;
 import com.ccicnavi.bims.akita.common.pojo.domain.AttaTmpDO;
 import com.ccicnavi.bims.akita.common.pojo.domain.AttaTmpDTO;
 import com.ccicnavi.bims.akita.dao.AttaDao;
+import com.ccicnavi.bims.akita.utils.FileUtils;
 import com.ccicnavi.bims.breeder.api.IdWorkerService;
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.github.tobato.fastdfs.service.TrackerClient;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
 
@@ -28,6 +31,7 @@ import java.util.List;
  * @Description:
  */
 @Service
+@Slf4j
 public class AkitaServiceImpl implements AkitaService {
     @Autowired
     private FastFileStorageClient fastFileStorageClient;
@@ -46,20 +50,19 @@ public class AkitaServiceImpl implements AkitaService {
         String attaId = idWorkerService.getId(new Date());
         String attaTmpId = idWorkerService.getId(new Date());
         try {
-            File file = enclosureVO.getFile();
-            FileInputStream inputStream = new FileInputStream(enclosureVO.getFile());
-            StorePath storePath = fastFileStorageClient.uploadFile(inputStream, file.length(), FilenameUtils.getExtension(file.getName()), null);
-            AttaDO attaDO = new AttaDO(attaId, storePath.getFullPath(), enclosureVO.getFileOldName(), file.getName().substring(file.getName().lastIndexOf(".")), "", storePath.getGroup(), enclosureVO.getCreateBy(), enclosureVO.getRemark());
+            InputStream inputStream = FileUtils.byte2Input(enclosureVO.getFile());
+            StorePath storePath = fastFileStorageClient.uploadFile(inputStream, enclosureVO.getLength(), FilenameUtils.getExtension(enclosureVO.getFileOldName()), null);
+            AttaDO attaDO = new AttaDO(attaId, storePath.getFullPath(), enclosureVO.getFileOldName(), enclosureVO.getExt(), "", storePath.getGroup(), enclosureVO.getCreateBy(), enclosureVO.getRemark());
             AttaTmpDO attaTmpDO = new AttaTmpDO(attaTmpId, enclosureVO.getBusinId(), attaId, enclosureVO.getRemark());
             boolean insertAtta = attaDao.insertAtta(attaDO, attaTmpDO);
             if (insertAtta) {
                 return true;
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("",e);
         } finally {
-        }
 
+        }
         return false;
     }
 
