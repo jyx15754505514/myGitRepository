ae78f5784e871689faf630dadd7061eba4ac8706
guojinxu <guojinxu@ccicln.com>
2018-11-28 15:14:37 +0800

    商品信息date 添加注解

--- middle-module/middle-product/middle-product-common/src/main/java/com/ccicnavi/bims/product/pojo/GoodsDO.java
+++ middle-module/middle-product/middle-product-common/src/main/java/com/ccicnavi/bims/product/pojo/GoodsDO.java
@@ -1,5 +1,6 @@
 package com.ccicnavi.bims.product.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
@@ -31,12 +32,14 @@ public class GoodsDO implements Serializable {
     /**删除标识（N正常Y删除）*/
     private String isDeleted;
     /**创建时间*/
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdTime;
     /**创建人id*/
     private String createdUuid;
     /**创建人*/
     private String createdName;
     /**最后一次更新时间*/
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updatedTime;
     /**更新人uuid*/
     private String updatedUuid;
