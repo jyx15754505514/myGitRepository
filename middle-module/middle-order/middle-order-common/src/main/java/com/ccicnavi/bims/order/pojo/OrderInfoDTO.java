5cdeff087e8025cd11eb34a0ee32d89060e92388
songyateng <songyateng@yuanh.net>
2018-11-29 09:59:23 +0800

    MOD 委托单添加字段

--- middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDTO.java
+++ middle-module/middle-order/middle-order-common/src/main/java/com/ccicnavi/bims/order/pojo/OrderInfoDTO.java
@@ -3,9 +3,7 @@ package com.ccicnavi.bims.order.pojo;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -34,227 +32,253 @@ public class OrderInfoDTO implements Serializable {
      */
     private String orderName;
     /**
-     * 委托日期
+     *委托日期
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date orderDate;
+    private String orderDate;
     /**
-     * 委托区域id
+     *委托区域id
      */
     private String orderAreaId;
     /**
-     * 外部编号
+     *外部编号
      */
     private String crmExternalNo;
     /**
-     * 客户id
+     *客户id
      */
     private String crmUuid;
     /**
-     * 客户名称
+     *客户名称
      */
     private String crmName;
     /**
-     * 联系人
+     *联系人
      */
     private String crmLinkman;
     /**
-     * 联系人电话
+     *联系人电话
      */
     private String crmTelphone;
     /**
-     * 联系人邮箱
+     *联系人邮箱
      */
     private String crmEmail;
     /**
-     * 贸易合同编号
+     *贸易合同编号
      */
     private String crmContractNo;
     /**
-     * 联检方
+     *联检方
      */
     private String thirdOrgName;
     /**
-     * 联检方联系人
+     *联检方联系人
      */
     private String thirdOrgLinkman;
     /**
-     * 联检方联系人电话
+     *联检方联系人电话
      */
     private String thirdOrgTelphone;
     /**
-     * 委托项目名称
+     *委托项目名称
      */
     private String orderItemName;
     /**
-     * 委托项目类型
+     *委托项目类型
      */
     private String orderItemType;
     /**
-     * 项目规格
+     *项目规格
      */
     private String orderItemSpec;
     /**
-     * 项目标识
+     *项目标识
      */
     private String orderItemSign;
     /**
-     * 数量
+     *数量
      */
-    private Integer orderItemQty;
+    private String orderItemQty;
     /**
-     * 数量单位
+     *数量单位
      */
     private String orderItemQtyUnit;
     /**
-     * 使用标准id
+     *使用标准id
      */
     private String useStdUuid;
     /**
-     * 使用标准名称
+     *使用标准名称
      */
     private String useStdName;
     /**
-     * 标准说明
+     *标准说明
      */
     private String useStdComment;
     /**
-     * 总费用
+     *总费用
      */
-    private Integer totalFee;
+    private String totalFee;
     /**
-     * 首次计费日期
+     *首次计费日期
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date firstFeeDate;
+    private String firstFeeDate;
     /**
-     * 是Y否N内部委托单
+     *是Y否N内部委托单
      */
     private String isInternal;
     /**
-     * 委托人来源
+     *委托人来源
      */
     private String orderCrmSource;
     /**
-     * 委托单来源机构id
+     *委托单来源机构id
      */
     private String sourceOrgUuid;
     /**
-     * 委托单来源机构名称
+     *委托单来源机构名称
      */
     private String sourceOrgName;
     /**
-     * 计划检验时间
+     *计划检验时间
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date planInspectDate;
+    private String planInspectDate;
     /**
-     * 计划检验地
+     *计划检验地
      */
     private String planInspectAddr;
     /**
-     * 备注
+     *备注
      */
     private String comments;
     /**
-     * 币种id
+     *币种id
      */
     private String currencyType;
     /**
-     * 结论方式
+     *结论方式
      */
     private String conclusionType;
     /**
-     * 证书语种id
+     *取证方式
+     */
+    private String fetchCertType;
+    /**
+     *邮寄地址
+     */
+    private String fetchCertPostal;
+    /**
+     *是Y否N带标证书，默认N
+     */
+    private String isLabelCert;
+    /**
+     *是Y否N电子证书，默认N
+     */
+    private String isDigitalCert;
+    /**
+     *是Y否N出证，默认N
+     */
+    private String isOpenCert;
+    /**
+     *证书语种id
      */
     private String certLangUuid;
     /**
-     * 证书正本数量
+     *证书正本数量
      */
-    private Integer certQty;
+    private String certQty;
     /**
-     * 证书副本数量
+     *证书副本数量
      */
-    private Integer certCopyQty;
+    private String certCopyQty;
     /**
-     * 受理日期
+     *受理日期
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date acceptDate;
+    private String acceptDate;
     /**
-     * 受理机构id
+     *受理机构id
      */
     private String acceptOrgUuid;
     /**
-     * 受理机构名称（公司名称）
+     *受理机构名称（公司名称）
      */
     private String acceptOrgName;
     /**
-     * 状态uuid
+     *状态uuid
      */
     private String statusUuid;
     /**
-     * 状态名称
+     *状态名称
      */
     private String statusName;
     /**
-     * 是Y否N分包标记，默认为N
+     *是Y否N分包标记，默认为N
      */
     private String lblSubpkg;
     /**
-     * 是Y否N执行标记，默认为N
+     *是Y否N执行标记，默认为N
      */
     private String lblExec;
     /**
-     * 委托方是Y否N具备专业能力，默认为N
+     *委托方是Y否N具备专业能力，默认为Y
      */
     private String lblCrmCapc;
     /**
-     * 是Y否N需要评审，默认为N
+     *是Y否N需要商务评审，默认为N
      */
-    private String isReview;
+    private String isReviewContract;
     /**
-     * 删除标识（N正常Y删除）
+     *是Y否N需要技术评审，默认为N
+     */
+    private String isReviewTech;
+    /**
+     *删除标识（N正常Y删除）
      */
     private String isDeleted;
     /**
-     * 创建时间
+     *创建时间
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date createdTime;
+    private String createdTime;
     /**
-     * 创建人id
+     *创建人id
      */
     private String createdUuid;
     /**
-     * 创建人
+     *创建人
      */
     private String createdName;
     /**
-     * 最后一次更新时间
+     *最后一次更新时间
      */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date updatedTime;
+    private String updatedTime;
     /**
-     * 更新人uuid
+     *更新人uuid
      */
     private String updatedUuid;
     /**
-     * 更新人
+     *更新人
      */
     private String updatedName;
     /**
-     * 产品线id
+     *产品线id
      */
     private String prodCatalogUuid;
     /**
-     * 归属机构id
+     *归属机构id
      */
     private String orgUuid;
     /**
-     * 应用系统id
+     *应用系统id
      */
     private String appSysUuid;
+
+
     /**
      * 委托单当前状态
      */
@@ -339,10 +363,6 @@ public class OrderInfoDTO implements Serializable {
      *提供单证的名称，以逗号区分
      */
     private String provideCertName;
-    /**
-     *是Y否N出证，默认N
-     */
-    private String isOpenCert;
     /**
      *服务项
      */
