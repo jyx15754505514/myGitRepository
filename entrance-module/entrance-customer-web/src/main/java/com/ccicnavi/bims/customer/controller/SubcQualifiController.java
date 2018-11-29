a505e846c5932ee39b108be3e1c07ce8befeafe0
柴言顺 <18333149648@163.com>
2018-11-29 18:34:01 +0800

    mod 分包方银行、资质，证书纸加日志

--- entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/controller/SubcQualifiController.java
+++ entrance-module/entrance-customer-web/src/main/java/com/ccicnavi/bims/customer/controller/SubcQualifiController.java
@@ -1,6 +1,7 @@
 package com.ccicnavi.bims.customer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.ccicnavi.bims.common.ResultCode;
 import com.ccicnavi.bims.common.ResultT;
 import com.ccicnavi.bims.customer.api.SubcQualifiService;
@@ -9,11 +10,13 @@ import com.ccicnavi.bims.customer.pojo.SubcQualifiDTO;
 import com.ccicnavi.bims.customer.pojo.SubcontractorDTO;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -34,21 +37,29 @@ public class SubcQualifiController {
     SubcQualifiService subcQualifiService;
 
     /**
-     * @author WangYingLing
-     * @description 查询分包方资质信息
-     * @param subcontractorDTO
-     * @return com.ccicnavi.bims.common.ResultT
-     */
+    *@Description: 查询分包方资质信息
+    *@Author: ChaiYanShun
+    *@Create: 2018/11/29 18:11
+    *@Param [subcontractorDTO]
+    *@return com.ccicnavi.bims.common.ResultT
+    */
     @RequestMapping(value = "/listSubcQuali",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT listSubcQuali(@RequestBody SubcontractorDTO subcontractorDTO){
-        List<SubcQualifiDO> subcQualifiDOList=null;
+        log.debug("查询分包方资质信息列表 Param:" + JSON.toJSONString(subcontractorDTO)  + " Time: " + new Date());
+        //判断分包方id是否为空
+        if(StringUtils.isEmpty(subcontractorDTO.getSubcUuid())){
+            return ResultT.failure(ResultCode.PARAM_IS_BLANK);
+        }
         try {
-            subcQualifiDOList=subcQualifiService.listSubcQuali(subcontractorDTO);
-            return ResultT.success(subcQualifiDOList);
+            List<SubcQualifiDO> subcQualifiDOList=subcQualifiService.listSubcQuali(subcontractorDTO);
+            if(subcQualifiDOList != null && subcQualifiDOList.size() > 0){
+                log.debug("获取分包方资质列表结果：" + JSON.toJSONString(subcQualifiDOList));
+                return ResultT.success(subcQualifiDOList);
+            }
         } catch (Exception e) {
             log.error("查询分包方资质信息失败",e);
-            return ResultT.failure(ResultCode.LIST_FAILURE);
         }
+        return ResultT.failure(ResultCode.LIST_FAILURE);
     }
 
     /**
@@ -60,10 +71,11 @@ public class SubcQualifiController {
     */
     @RequestMapping(value = "/saveSubcQuali",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT saveSubcQuali(@RequestBody SubcQualifiDTO subcQualifiDTO){
-        int count=0;
+        log.debug("新增分包方资质信息 Param:" + JSON.toJSONString(subcQualifiDTO)  + " Time: " + new Date());
         try {
-            count=subcQualifiService.insertSubcQuali(subcQualifiDTO);
-            if(count>0){
+            Integer num =subcQualifiService.insertSubcQuali(subcQualifiDTO);
+            if(num != null && num != 0) {
+                log.debug("新增分包方资质信息结果：保存数量" + num);
                 return ResultT.success("新增分包方资质信息成功");
              }
         } catch (Exception e) {
@@ -73,17 +85,19 @@ public class SubcQualifiController {
      }
 
     /**
-     * @author WangYingLing
-     * @description 删除分包方资质信息
-     * @param subcQualifiDTO
-     * @return com.ccicnavi.bims.common.ResultT
-     */
+    *@Description: 删除分包方资质信息
+    *@Author: ChaiYanShun
+    *@Create: 2018/11/29 18:14
+    *@Param [subcQualifiDTO]
+    *@return com.ccicnavi.bims.common.ResultT
+    */
     @RequestMapping(value = "/removeSubcQuali",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT removeSubcQuali(@RequestBody SubcQualifiDTO subcQualifiDTO){
-        int count=0;
+        log.debug("删除分包方资质信息 Param:" + JSON.toJSONString(subcQualifiDTO)  + " Time: " + new Date());
         try {
-            count=subcQualifiService.deleteSubcQuali(subcQualifiDTO);
-            if(count > 0){
+            Integer num=subcQualifiService.deleteSubcQuali(subcQualifiDTO);
+            if(num != null && num != 0) {
+                log.debug("删除分包方资质信息结果：删除数量" + num);
                 return ResultT.success("删除分包方资质信息成功");
             }
         } catch (Exception e) {
@@ -93,17 +107,19 @@ public class SubcQualifiController {
     }
 
     /**
-     * @author WangYingLing
-     * @description 修改分包方资质信息
-     * @param subcQualifiDO
-     * @return com.ccicnavi.bims.common.ResultT
-     */
+    *@Description: 修改分包方资质信息
+    *@Author: ChaiYanShun
+    *@Create: 2018/11/29 18:16
+    *@Param [subcQualifiDO]
+    *@return com.ccicnavi.bims.common.ResultT
+    */
     @RequestMapping(value = "/updateSubcQuali",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT updateSubcQuali(@RequestBody SubcQualifiDO subcQualifiDO){
-        int count=0;
+        log.debug("开始修改分包方资质信息 Param:" + JSON.toJSONString(subcQualifiDO)  + " Time: " + new Date());
         try {
-            count=subcQualifiService.updateSubcQuali(subcQualifiDO);
-            if(count > 0){
+            Integer num=subcQualifiService.updateSubcQuali(subcQualifiDO);
+            if(num != null && num != 0) {
+                log.debug("修改分包方资质信息结果：更新数量" + num);
                 return ResultT.success("修改分包方资质信息成功");
             }
         } catch (Exception e) {
@@ -113,17 +129,19 @@ public class SubcQualifiController {
     }
 
     /**
-     * @author WangYingLing
-     * @description 获取分包方资质信息
-     * @param subcQualifiDORequstBody
-     * @return com.ccicnavi.bims.common.ResultT
-     */
+    *@Description: 获取分包方资质信息
+    *@Author: ChaiYanShun
+    *@Create: 2018/11/29 18:18
+    *@Param [subcQualifiDORequstBody]
+    *@return com.ccicnavi.bims.common.ResultT
+    */
     @RequestMapping(value = "/getSubcQuali",method = RequestMethod.POST,produces = "application/json;charset=UTF-8")
     public ResultT getSubcQuali(@RequestBody SubcQualifiDO subcQualifiDORequstBody){
-        SubcQualifiDO subcQualifiDO=new SubcQualifiDO();
+        log.debug("开始获取分包方资质信息 Param:" + JSON.toJSONString(subcQualifiDORequstBody)  + " Time: " + new Date());
         try {
-            subcQualifiDO=subcQualifiService.getSubcQuali(subcQualifiDORequstBody);
+            SubcQualifiDO subcQualifiDO=subcQualifiService.getSubcQuali(subcQualifiDORequstBody);
             if(subcQualifiDO != null) {
+                log.debug("获取分包方资质信息：" + subcQualifiDO);
                 return ResultT.success(subcQualifiDO);
             }
         } catch (Exception e) {
